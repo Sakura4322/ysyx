ac945c810442c5e14a98e6457fe942d6f0ff351e
tracer-ysyx <tracer@ysyx.org>
2024-07-25 14:54:35 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:54:35 up  7:49,  1 user,  load average: 0.51, 0.32, 0.28

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -70,8 +70,8 @@ static int cmd_si(char *args) {
 
     return 0; // 返回0表示执行成功  
 }  
-
-/*static int info(char *arg){
+/*
+/static int info(char *arg){
 if (*arg==r){
 
 }if (*arg==w){
@@ -86,8 +86,8 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-{"si[N]","让程序单步执行N条指令后暂停执行,当N没有给出时, 缺省为1",cmd_si}
-/*{"info SUBCMD","打印寄存器状态,打印监视点信息",cmd_info},
+{"si","让程序单步执行N条指令后暂停执行,当N没有给出时, 缺省为1",cmd_si}
+/*{"info ","打印寄存器状态,打印监视点信息",cmd_info},
 {"x N EXPR","求出表达式EXPR的值, 将结果作为起始内存地址, 以十六进制形式输出连续的N个4字节",cmd_x},
 {"p EXPR","求出表达式EXPR的值, EXPR支持的运算请见调试中的表达式求值小节",cmd_p},
 {"w EXPR","当表达式EXPR的值发生变化时, 暂停程序执行",cmd_w},
