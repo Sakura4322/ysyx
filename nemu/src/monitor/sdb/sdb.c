99e6e1d4ab187fe9de0cb01aa46376d0212f4a6d
tracer-ysyx <tracer@ysyx.org>
2024-07-25 14:20:18 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:20:18 up  7:14,  1 user,  load average: 0.27, 0.24, 0.19

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -43,6 +43,7 @@ static char* rl_gets() {
 }
 
 static int cmd_c(char *args) {
+
   cpu_exec(-1);
   return 0;
 }
@@ -54,6 +55,29 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+
+static int cmd_si(char *args) {  
+    int n = 1; // 默认单步执行1条指令  
+
+    // 检查参数是否有效，提取N的值  
+    if (args != NULL && sscanf(args, "%d", &n) != 1) {  
+        printf("Invalid argument for si. Expecting an integer.\n");  
+        return -1;  
+    }  
+
+    // 单步执行 n 条指令  
+    cpu_exec(n);
+
+    return 0; // 返回0表示执行成功  
+}  
+
+/*static int info(char *arg){
+if (*arg==r){
+
+}if (*arg==w){
+
+}
+}*/
 static struct {
   const char *name;
   const char *description;
@@ -62,7 +86,12 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+{"si[N]","让程序单步执行N条指令后暂停执行,当N没有给出时, 缺省为1",cmd_si}
+/*{"info SUBCMD","打印寄存器状态,打印监视点信息",cmd_info},
+{"x N EXPR","求出表达式EXPR的值, 将结果作为起始内存地址, 以十六进制形式输出连续的N个4字节",cmd_x},
+{"p EXPR","求出表达式EXPR的值, EXPR支持的运算请见调试中的表达式求值小节",cmd_p},
+{"w EXPR","当表达式EXPR的值发生变化时, 暂停程序执行",cmd_w},
+{"d N","删除序号为N的监视点",cmd_d}*/
   /* TODO: Add more commands */
 
 };
