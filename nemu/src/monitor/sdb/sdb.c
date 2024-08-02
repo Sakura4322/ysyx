1932c7ae71f6f04a224011ea525693627a3e8659
tracer-ysyx <tracer@ysyx.org>
2024-08-02 21:39:57 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:39:57 up 1 day, 21:42, 10 users,  load average: 0.71, 0.39, 0.26

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -112,7 +112,7 @@ static struct {
 
   /* TODO: Add more commands */
 {"si","让程序单步执行N条指令后暂停执行,当N没有给出时, 缺省为1",cmd_si},
-{"info ","打印寄存器状态,打印监视点信息",cmd_info},
+{"info","打印寄存器状态,打印监视点信息",cmd_info},
 {"x","求出表达式EXPR的值, 将结果作为起始内存地址, 以十六进制形式输出连续的N个4字节",cmd_x},
 /*
 {"p EXPR","求出表达式EXPR的值, EXPR支持的运算请见调试中的表达式求值小节",cmd_p}
