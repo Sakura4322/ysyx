8718a0e73e3ce2a514dfd25bb9e8af72be2e4de9
tracer-ysyx <tracer@ysyx.org>
2024-09-01 00:19:52 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:19:52 up 1 day, 19:38,  1 user,  load average: 0.31, 0.40, 0.39

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -100,7 +100,7 @@ static bool make_token(char *e) {
 
   nr_token = 0;
 
-  while (e[position]+1 != '\0') {   
+  while (e[position+1] != '\0') {   
     
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
