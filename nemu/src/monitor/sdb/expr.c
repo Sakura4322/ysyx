ed56ab1748387c5fafb7b0f8b023c0d767c862f9
tracer-ysyx <tracer@ysyx.org>
2024-09-01 01:35:33 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:35:33 up 1 day, 20:54,  1 user,  load average: 0.89, 0.59, 0.43

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -134,10 +134,7 @@ static bool make_token(char *e) {
 				strncpy(tokens[nr_token].str,substr_start,substr_len);
 				nr_token++;
 			
-                } else {  
-                    printf("Error: token array is full.\n");  
-                    return false; 
-                }  
+                } 
          //当tokens长度小于32位才开始录入，如果是非数字字符类型，就只录入类型，否则再录入数值
          
          
