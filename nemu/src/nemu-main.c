765bf83f83b81061215f2da46965af6c82cab94b
tracer-ysyx <tracer@ysyx.org>
2024-09-01 01:28:25 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:28:25 up 1 day, 20:47,  1 user,  load average: 0.49, 0.50, 0.38

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -48,8 +48,8 @@ while (fgets(buf,sizeof(buf),fp) != NULL){
 		bool success_prt = true;
 		bool *success= &success_prt;
 		 int test_num = expr(epr,success);
-		//printf("%d\n%s\n%d\n",test_num,epr,test_num);
-		assert(num==test_num);
+		printf("%d\n%s\n%d\n",num,epr,test_num);
+		//assert(num==test_num);
 /*
 if (num==test_num)printf("yes\n");
 else printf("No\n");
