f2f2244b1ce1d9ba2b66366bc8b91bcd5839f8c8
tracer-ysyx <tracer@ysyx.org>
2024-09-01 15:24:06 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:24:06 up 1 day, 23:43,  1 user,  load average: 0.47, 0.35, 0.33

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -173,11 +173,11 @@ static bool check_parenthese(int p, int q) {
 			if (tokens[i].type == TK_LPAREN) {
 				pd++;
 				//l = i;
-				printf("%d",pd);
+				printf("%d\n",pd);
 			} else if (tokens[i].type == TK_RPAREN) {
 				pd--;
 				//r = i;
-				printf("%d",pd);
+				printf("%d\n",pd);
 			}
 			if (pd < 0 || (i == q && pd != 0)) {
 			
