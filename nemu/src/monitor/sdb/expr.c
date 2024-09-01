7d3a5472cbe835db62aab0b9b5da870da382506a
tracer-ysyx <tracer@ysyx.org>
2024-09-02 00:36:52 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:36:52 up 2 days,  8:56,  1 user,  load average: 0.21, 0.31, 0.48

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -135,7 +135,8 @@ static bool make_token(char *e) {
                     
                      tokens[nr_token].type=rules[i].token_type;				
                      strncpy(tokens[nr_token].str,substr_start,substr_len);
-                    // printf("%s\n",tokens[nr_token].str);//测试输入表达式是否正确
+                     printf("nr_token= %d tokens = %s\n",nr_token,tokens[nr_token].str);
+                     //测试输入表达式是否正确
 				nr_token++;
 			
                 } else {  
