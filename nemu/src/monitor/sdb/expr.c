76badaab5ef60e859eff28791a104084a0f0e8aa
tracer-ysyx <tracer@ysyx.org>
2024-09-01 13:40:37 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:40:37 up 1 day, 22:00,  1 user,  load average: 0.71, 0.43, 0.44

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -287,7 +287,7 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
    int p=0,q=nr_token-1;
    int num=eval(p,q);
-   printf("%d",num);
+   printf("%u",num);
   
   /*
   for (i = 0; i < nr_token; i ++) {
