ae90cb82f50121996ef7a9c585e1d3fece6c7967
tracer-ysyx <tracer@ysyx.org>
2024-09-02 01:13:24 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:13:24 up 2 days,  9:33,  1 user,  load average: 0.51, 0.41, 0.48

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -137,7 +137,7 @@ static bool make_token(char *e) {
                      strncpy(tokens[nr_token].str,substr_start,substr_len);
                      tokens[nr_token].str[substr_len]='\0';
                      
-                     printf("nr_token= %d tokens = %s\n",nr_token,tokens[nr_token].str);
+                     //printf("nr_token= %d tokens = %s\n",nr_token,tokens[nr_token].str);
                      //测试输入表达式是否正确
 				nr_token++;
 			
@@ -179,11 +179,11 @@ static bool check_parenthese(int p, int q) {
 			if (tokens[i].type == TK_LPAREN) {
 				pd++;
 				
-				//printf("%d %d %d\n",i,q,pd);测试点，测试括号匹配
+				printf("%d %d %d\n",i,q,pd);//测试点，测试括号匹配
 			} else if (tokens[i].type == TK_RPAREN) {
 				pd--;
 				
-				//printf("%d %d %d\n",i,q,pd);
+				printf("%d %d %d\n",i,q,pd);
 			}
 			if (pd < 0 || (i == q && pd != 0)) {
 			
