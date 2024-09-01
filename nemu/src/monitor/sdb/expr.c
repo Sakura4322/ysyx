d2c97776f32c4607ed521d3c7cbc259b15c8c4f3
tracer-ysyx <tracer@ysyx.org>
2024-09-02 01:15:27 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:15:27 up 2 days,  9:35,  1 user,  load average: 0.58, 0.49, 0.50

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -179,11 +179,11 @@ static bool check_parenthese(int p, int q) {
 			if (tokens[i].type == TK_LPAREN) {
 				pd++;
 				
-				printf("%d %d %d\n",i,q,pd);//测试点，测试括号匹配
+				//printf("%d %d %d\n",i,q,pd);测试点，测试括号匹配
 			} else if (tokens[i].type == TK_RPAREN) {
 				pd--;
 				
-				printf("%d %d %d\n",i,q,pd);
+				//printf("%d %d %d\n",i,q,pd);
 			}
 			if (pd < 0 || (i == q && pd != 0)) {
 			
@@ -276,7 +276,7 @@ printf("DIVISION BY ZERO");
 //exit(EXIT_FAILURE); // 终止程序并返回失败状态
 return 0;	
 	}
-	//printf("%d %d %d\n",val1,op_type,val2); 测试点，计算的元素和符号
+	printf("%d %d %d\n",val1,op_type,val2); //测试点，计算的元素和符号
 	
     switch (op_type) {
       case TK_PLUS: return val1 + val2;
