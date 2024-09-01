efb993842b5d3440dca57d6b9a711a6dda5bab2a
tracer-ysyx <tracer@ysyx.org>
2024-09-01 13:17:36 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:17:36 up 1 day, 21:37,  1 user,  load average: 0.24, 0.57, 0.57

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]) {
 #endif
 	bool success_prt = true;
 	bool *success= &success_prt;
-	char epr[]="(((1+5*2)*7))+1+(1)*8";
+	char epr[]="(((1+5*2)*(7)))";
 	printf("%s\n",epr);
 	 expr(epr,success);
   
