2e35d81b39ffc3f09a1f25e38eb108e72150f213
tracer-ysyx <tracer@ysyx.org>
2024-09-02 00:25:37 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:25:37 up 2 days,  8:45,  1 user,  load average: 0.45, 0.67, 0.68

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -103,16 +103,16 @@ static bool make_token(char *e) {
   nr_token = 0;
 
   while (e[position] != '\0') {   
- 
+ /*
   if (e[position] == '\n'){
   	for (int j=0;j<position;j++){
 		printf("%s",tokens[j].str);
 		}
-		//测试点，测试输入的表达式是否正确
+		测试点，测试输入的表达式是否正确
 		
   	break;
   	}
- 
+  */
   	
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
@@ -135,6 +135,7 @@ static bool make_token(char *e) {
                     
                      tokens[nr_token].type=rules[i].token_type;				
                      strncpy(tokens[nr_token].str,substr_start,substr_len-1);
+                     printf("%s\n",tokens[nr_token].str);
 				nr_token++;
 			
                 } else {  
