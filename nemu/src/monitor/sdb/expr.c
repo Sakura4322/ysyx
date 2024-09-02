91df7fff52705f39622e865c47af60582fa3d947
tracer-ysyx <tracer@ysyx.org>
2024-09-02 11:50:52 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:50:52 up 2 days, 11:15,  1 user,  load average: 0.52, 1.37, 2.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -246,7 +246,7 @@ static Token find_op(int p,int q){
 	}
 
 //递归计算表达式
-static uint32_t eval(int p,int q) {
+static int eval(int p,int q) {
   if (p > q) {
     /* Bad expression */
     printf("Bad expression");
@@ -269,8 +269,8 @@ static uint32_t eval(int p,int q) {
   Token result=find_op(p,q);
     int op = atoi(result.str);
     int op_type = result.type;
-    uint32_t val1 = eval(p, op - 1);
-    uint32_t val2 = eval(op + 1, q);
+    int val1 = eval(p, op - 1);
+    int val2 = eval(op + 1, q);
 if (val2==0&&op_type==TK_DIVIDE){
 printf("DIVISION BY ZERO");
 //exit(EXIT_FAILURE); // 终止程序并返回失败状态
