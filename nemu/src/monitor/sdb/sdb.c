04730004be613f0f1a16b8b15f6a3738a1ee7456
tracer-ysyx <tracer@ysyx.org>
2024-09-03 13:12:57 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:12:57 up 2 days, 18:32,  1 user,  load average: 0.86, 0.49, 0.42

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -112,7 +112,8 @@ static int cmd_x(char *args) { // 扫描内存
 static int cmd_p(char *args){//表达式求值
 	bool success_prt = true;
 	bool *success= &success_prt;
-	printf ("%d",expr(args , success));
+	printf ("%s\n",args);
+	printf ("%d\n",expr(args , success));
 	return 0;
 }
 /*
