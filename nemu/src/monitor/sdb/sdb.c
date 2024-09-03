b3efc1a5d362063b9e2d2658a9f053b43b699c70
tracer-ysyx <tracer@ysyx.org>
2024-09-03 20:01:46 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:01:46 up 3 days,  1:17,  1 user,  load average: 0.86, 0.29, 0.16

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -113,7 +113,7 @@ static int cmd_p(char *args){//表达式求值
 	bool success_prt = true;
 	bool *success= &success_prt;
 	printf ("成功进行到执行命令函数，输入表达式为 ： %s\n",args);
-	int ans=expr(args ,success);
+	int ans=expr(args,success);
 	if (*success){
 	printf ("成功计算答案为 ： %d\n",ans);
 	}
