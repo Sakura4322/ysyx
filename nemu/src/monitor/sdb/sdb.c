c4d87c888bab84a44000c69eb355746a78756dab
tracer-ysyx <tracer@ysyx.org>
2024-09-04 00:18:55 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:18:55 up 3 days,  5:35,  1 user,  load average: 0.40, 0.57, 0.54

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -86,11 +86,11 @@ char *arg = strtok(NULL , " ");
 }  
 
 static int cmd_info(char *args){
-char *arg=strtok(NULL," ");
-if (strcmp(arg,"r")==0){//打印寄存器信息
+printf("%s",args);
+if (strcmp(args,"r")==0){//打印寄存器信息
 isa_reg_display();
 }
-if (strcmp(arg,"w")==0){
+if (strcmp(args,"w")==0){
 	display_wp();
 	}
 return 0;
