8043fb05f5a5ff1536fea38008314f0ec741ba42
tracer-ysyx <tracer@ysyx.org>
2024-09-03 16:42:29 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:42:29 up 2 days, 21:58,  1 user,  load average: 0.23, 0.29, 0.32

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -252,7 +252,7 @@ printf ("进入find_op函数成功\n");
 word_t paddr_read(paddr_t addr, int len);
 //递归计算表达式
 static int eval(int p,int q,bool *success) {
-if (!*success)return 0;
+if (*success==false)return 0;
 printf ("成功进入eval 函数 p = %d q = %d\n",p,q);
 printf ("准备进入括号匹配 p = %d q = %d\n",p,q);
   if (p > q) {
@@ -348,7 +348,7 @@ printf ("成功make_tokens\n");
 int p=0,q=nr_token-1;
 printf ("准备进入计算 p=%d q=%d\n",p,q);
    int num=eval(p,q,success);
-   if (!success )return 0;
+   if (*success==false )return 0;
    printf("Ans = %u\n",num);
    
 
