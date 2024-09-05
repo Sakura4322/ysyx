aeea3721cc82a897f89b2e36a6b1c106eceb4b62
tracer-ysyx <tracer@ysyx.org>
2024-09-05 23:36:07 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:36:07 up 3 days, 14:40,  3 users,  load average: 0.46, 0.35, 0.36

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -37,7 +37,10 @@ void isa_reg_display() {
 word_t isa_reg_str2val(const char *s, bool *success) {
 		printf("成功读取寄存器数值");
 		for(int i=0;i<num_regs;i++){
-			if (strcmp(s,regs[i])==0)return cpu.gpr[i];
+			if (strcmp(s,regs[i])==0){
+			printf("s : %d",cpu.gpr[i]);
+				return cpu.gpr[i];
+				}
 			}
 			*success = false ;
 			return 0;
