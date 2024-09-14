898ad3622886d42026e8f264d4473c27e4573109
tracer-ysyx <tracer@ysyx.org>
2024-09-14 10:06:53 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:06:53 up 35 min,  1 user,  load average: 0.28, 0.32, 0.38

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -386,7 +386,7 @@ word_t expr(char *e, bool *success) {
   for (int i = 0; i < nr_token; i ++) {
   if (tokens[i].type == TK_MULTIPLY && (i == 0 || tokens[i - 1].type != TK_VALUE||tokens[i - 1].type !=TK_RPAREN) ) {
     tokens[i].type = DEREF;
-    //printf("成功是别解引用符");
+    printf("成功是别解引用符");
   } 
 }
 
