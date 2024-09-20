ea5be45eb4c6e03871faad11147d705bce5d5e36
tracer-ysyx <tracer@ysyx.org>
2024-09-20 21:56:18 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:56:18 up  1:56,  1 user,  load average: 0.42, 0.34, 0.20

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]) {
 #endif
 
 
-
+/*
 FILE *fp = fopen("/home/sakura/ysyx-workbench/nemu/tools/gen-expr/build/input11","r");
 assert (fp != NULL);
 int yes=0,no=0;
@@ -59,7 +59,7 @@ while (fgets(buf,sizeof(buf),fp) != NULL){
 	}
 	printf ("yes=%d\nno=%d\n",yes ,no);
 	fclose(fp);
-	
+*/	
   /* Start engine. */
   engine_start();
   
