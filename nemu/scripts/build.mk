df74fcb863dc49f3092decb25fae843d11e25d46
tracer-ysyx <tracer@ysyx.org>
2024-09-25 19:30:51 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:30:51 up 1 day, 6 min,  1 user,  load average: 0.36, 0.31, 0.28

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -27,20 +27,20 @@ LDFLAGS := -O2 $(LDFLAGS)
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
-PREPROCESS_DIR = $(BUILD_DIR)/preprocess
+PREPROCESS_DIR = $(NEMU_HOME)/expand_files
 
 # Compilation patterns
 $(OBJ_DIR)/%.o: %.c
 	@echo + CC $<
 	@mkdir -p $(dir $@)
-	@$(CC) $(CFLAGS) -E -o $*.i $<
+	@$(CC) $(CFLAGS) -E -o $< $(PREPROCESS_DIR)/$*.i
 	@$(CC) $(CFLAGS) -c -o $@ $<
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 $(OBJ_DIR)/%.o: %.cc
 	@echo + CXX $<
 	@mkdir -p $(dir $@)
-	@$(CXX) $(CFLAGS) -E -o $*.i $<
+	@$(CXX) $(CFLAGS) -E -o $< $(PREPROCESS_DIR)/$*.i
 	@$(CXX) $(CFLAGS) $(CXXFLAGS) -c -o $@ $<
 	$(call call_fixdep, $(@:.o=.d), $@)
 
