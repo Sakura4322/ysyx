958a0227f1a44ffdcd0c7bcc94f568b7cee76376
tracer-ysyx <tracer@ysyx.org>
2024-10-02 15:28:55 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:28:55 up 1 day, 22:55,  1 user,  load average: 0.14, 0.16, 0.11

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -33,7 +33,7 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)//用于处理高位立即数，用于地址计算所以需要先进位
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immJ() do { *imm = ((((((SEXT(BITS(i, 31, 31), 1) << 8) | BITS(i, 19, 12)) << 1) |BITS(i, 20, 20)) << 10) | BITS(i, 30, 21))<<1; } while(0)
+#define immJ() do { *imm = ((((((SEXT(BITS(i, 31, 31), 1) << 8) | BITS(i, 19, 12)) << 1) |BITS(i, 20, 20)) << 10) | BITS(i, 30, 21)); } while(0)
 #define immB() do { *imm = (((((((SEXT(BITS(i,31,31),1))<<1)|BITS(i,7,7))<<6)|BITS(i,30,25))<<4)|BITS(i,11,8))<<1; }while(0)
 
 
