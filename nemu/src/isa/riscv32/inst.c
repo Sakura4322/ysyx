7f62419bd6d683824c89298463ebe794826200a1
tracer-ysyx <tracer@ysyx.org>
2024-11-12 18:16:31 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:16:31 up  1:57,  1 user,  load average: 0.22, 0.11, 0.10

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -34,8 +34,7 @@ enum {
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)//用于处理高位立即数，用于地址计算所以需要先进位
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { *imm = ((((((SEXT(BITS(i, 31, 31), 1) << 8) | BITS(i, 19, 12)) << 1) |BITS(i, 20, 20)) << 10) | BITS(i, 30, 21))<<1; } while(0)
-#define immB() do { *imm = (((((((SEXT(BITS(i,31,31),1))<<1)|BITS(i,7,7))<<6)|BITS(i,30,25))<<4)|BITS(i,11,8))<<1; }while(0)
-
+#define immB() do {*imm = SEXT((BITS(i,31,31)<<1)|(BITS(i,7,7)<<6)|(BITS(i,30,25)<<4),12);}while(0)
 
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
