5761e1284e00f59bae21bfd96de237899629fd60
tracer-ysyx <tracer@ysyx.org>
2024-11-13 22:29:57 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:29:57 up  3:44,  1 user,  load average: 0.21, 0.14, 0.21

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -35,7 +35,7 @@ enum {
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { *imm = ((((((SEXT(BITS(i, 31, 31), 1) << 8) | BITS(i, 19, 12)) << 1) |BITS(i, 20, 20)) << 10) | BITS(i, 30, 21))<<1; } while(0)
 #define immB() do {*imm = SEXT((BITS(i,31,31)<<1)|(BITS(i,7,7)<<6)|(BITS(i,30,25)<<4)|BITS(i,11,8),12)<<1;\
-	/*printf("imm=%08x \n",(uint32_t)*imm);*/\
+	printf("imm=%08x \n",(uint32_t)*imm);\
 }while(0)
 
 
