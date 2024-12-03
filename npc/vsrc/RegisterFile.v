00de118186227fcd28b7a42a716f2b965f9c25bb
tracer-ysyx <tracer@ysyx.org>
2024-12-03 16:41:49 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:41:49 up 2 days,  4:11,  1 user,  load average: 0.23, 0.39, 0.52

--- npc/vsrc/RegisterFile.v
+++ npc/vsrc/RegisterFile.v
@@ -6,7 +6,7 @@ module RegisterFile #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
 	input ren1,
 	input ren2,
   input [ADDR_WIDTH-1:0] raddr1,raddr2,
-	output reg[DATA_WIDTH-1:0] rdata1,rdata2
+	output [DATA_WIDTH-1:0] rdata1,rdata2
 );
   reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0];
   always @(posedge clk) begin
