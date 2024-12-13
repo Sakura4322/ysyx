8d5741949ffbf8b753e37eaa576c9fe0919e618d
tracer-ysyx <tracer@ysyx.org>
2024-12-13 22:25:29 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:25:29 up 2 days, 14:52,  1 user,  load average: 0.55, 0.45, 0.38

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,16 +6,56 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
+
   panic("Not implemented");
 }
+
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+
+char *to_string(char *out,int a){
+ char temp[20];
+ int i=0;
+ while (a>0){
+		*(temp+i)=a%10+'0';
+		a/=10;
+		i++;
+ }
+i=strlen(temp);
+for(int j=0;j<i;j++){
+		*(out+j)=*(temp+i-j-1);
+	}
+return out;
 }
+
+
+int sprintf(char *out, const char *fmt, ...) {
+va_list args;
+va_start(args,fmt);
+for(int i=0;i<strlen(fmt);i++){
+if (*(fmt+i)=='%'){
+		char temp=*(fmt+i+1);
+		if(temp=='s'){
+			strcat(out,va_arg(args,char *));
+		}else if(temp=='d'){
+			int num=va_arg(args,int);
+			char temp_str[20];
+			strcat(out,to_string(temp_str,num));
+		}
+}	else{
+*(out+i)=*(fmt+i);	
+}
+}
+
+va_end(args);
+
+return 0;
+}
+
+
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
