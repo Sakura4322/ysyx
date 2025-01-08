94f8d6d9ab9aca85fc237b1ef349b1958841c07d
tracer-ysyx <tracer@ysyx.org>
2025-01-08 11:29:54 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:29:53 up 3 days,  5:48,  8 users,  load average: 1.15, 0.69, 0.46

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -62,9 +62,9 @@ static uint8_t *pmem = NULL;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-#if   defined(CONFIG_MTRACE)
+//#if   defined(CONFIG_MTRACE)
 	log_write("have  written in :%08x : %08x\n",(int)addr,(int)data);
-#endif
+//#endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
