4708ec922fb3fdede6a6777815e365775cc9c015
tracer-ysyx <tracer@ysyx.org>
2025-01-08 16:36:40 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:39 up 3 days,  8:11,  8 users,  load average: 0.86, 0.45, 0.42

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -129,11 +129,11 @@ static void execute(uint64_t n) {
 			 cout_pc_num-=1;
 	for(int i=0;i<=cout_pc_num%20;i++){
 	if(i==cout_pc_num){
- log_write("\n\n\n\nthe reg information : \n%s\n",*(iringbuf_reg_state-1%2));
+ log_write("\n\n\n\nthe reg information : \n%s\n",*(iringbuf_reg_state+cout_pc_num-1%2));
  log_write("//////////////////////////////////////the wrong ///////////////////////////////////\n"); 		
 	}	
  log_write("%s\n", *(iringbuf+i)); 
-	if(i==cout_pc_num-1){
+	if(i==cout_pc_num){
  log_write("//////////////////////////////////////the wrong ///////////////////////////////////\n"); 		
  log_write("%s\n",*(iringbuf_reg_state+cout_pc_num%2));
 	}	
