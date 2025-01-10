c5f00d230f0cb9a8ae5a2571fda3233ab18ce9bb
tracer-ysyx <tracer@ysyx.org>
2025-01-10 17:39:59 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:39:59 up 12:53, 15 users,  load average: 0.56, 0.57, 0.33

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -67,7 +67,7 @@ static long load_img() {
 }
 
 static int parse_args(int argc, char *argv[]) {
-	printf("this is this : %s",*(argv+1));
+	printf("this is this : %s",*(argv+2));
   const struct option table[] = {
     {"batch"    , no_argument      , NULL, 'b'},
     {"log"      , required_argument, NULL, 'l'},
