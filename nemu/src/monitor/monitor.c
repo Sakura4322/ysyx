bf409605aa754d0b3a85fd2e38531c4514a1ab63
tracer-ysyx <tracer@ysyx.org>
2025-01-10 21:37:32 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:37:32 up 15:33, 15 users,  load average: 0.13, 0.05, 0.07

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -88,11 +88,9 @@ static int parse_args(int argc, char *argv[]) {
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
       case 1:
-							//char *suffix=NULL;
 							char temp[256]={0};
 							strcpy(temp,optarg);
 							strtok(temp,".");
-							//printf("addr is : %s\n",suffix);
 							img_file=(strcmp(temp+1,"bin")==0)?NULL:optarg;
 							elf_file=(strcmp(temp+1,"elf")==0)?NULL:optarg;
 
