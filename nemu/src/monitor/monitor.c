5891d16e9160acf6fd3ad323a13d56fec631f389
tracer-ysyx <tracer@ysyx.org>
2025-01-11 15:45:54 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:45:54 up 20:16, 15 users,  load average: 0.15, 0.25, 0.34

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -167,7 +167,7 @@ static Elf32_Shdr (*parse_shdr(Elf32_Ehdr *ehdr,char *elf_file))[20]{
 
 			fseek(fp,start_addr_shdr,SEEK_SET);
 			int ret=fread(shdr,size_shdr,sections_num,fp);
-			if (ret!=size_shdr){
+			if (ret!=sections_num){
 			printf("Faild to read section header\n");	
 			printf("imformation about ret : %d\n",ret);	
 			printf("imformation about size_shdr : %lu\n",size_shdr);	
