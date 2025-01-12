d6e9f64c49e9f0e678dbef01c510936dc691ea0e
tracer-ysyx <tracer@ysyx.org>
2025-01-12 20:52:18 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:52:18 up 1 day,  7:41, 20 users,  load average: 0.33, 0.23, 0.20

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -217,7 +217,7 @@ char **parse_strtab(Elf32_Shdr *shdr,char *elf_file){
 					cnt++;
 				}
 			}	
-			strtab_printf(string,sym_num-6);
+			strtab_printf(string,sym_num-7);
 return string;
 }
 void sym_printf(Elf32_Sym *sym,int sym_num) {
