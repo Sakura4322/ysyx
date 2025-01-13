ed10eee3f4d73568e728123af7e08053b9dc5980
tracer-ysyx <tracer@ysyx.org>
2025-01-13 10:59:44 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:59:44 up 1 day,  9:50, 20 users,  load average: 0.26, 0.26, 0.18

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -128,7 +128,7 @@ Addr_Imfo *read_sym_func(Elf32_Shdr *shdr,Elf32_Sym *sym,char **strtab,int cnt_g
 
 					cnt=0;
 					for(int i=0;i<sym_num;i++){
-					if(sym[i].st_info==STT_FUNC){
+					if(ELF32_ST_TYPE(sym[i].st_info)==STT_FUNC){
 					func_addr[cnt].func_name=strtab[sym[i].st_name];
 					func_addr[cnt].start=sym[i].st_value;
 					func_addr[cnt].end=sym[i].st_value+sym[i].st_size;
