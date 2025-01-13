4ada35f67eb19348c783315fad245f0bb558a325
tracer-ysyx <tracer@ysyx.org>
2025-01-13 18:57:46 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:57:46 up 1 day, 16:22, 20 users,  load average: 0.58, 0.23, 0.15

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -196,7 +196,7 @@ if (addr[rsp].end <= s->dnpc || addr[rsp].start >s->dnpc ){
 
 		if(rsp>=1&&(s->dnpc>=addr[rsp-1].start&&s->dnpc<addr[rsp-1].end)){
 		rsp--;	
-		printf("0x%08x ret %s\trsp: %d\n",s->pc,addr[rsp].func_name,rsp);	
+		//printf("0x%08x ret %s\trsp: %d\n",s->pc,addr[rsp].func_name,rsp);	
 		log_write("0x%08x ret %s\n",s->pc,addr[rsp].func_name);	
 		return ;
 		}//pd ret
@@ -209,7 +209,7 @@ if (addr[rsp].end <= s->dnpc || addr[rsp].start >s->dnpc ){
 			addr[rsp].func_name=func_addr[i].func_name;
 			addr[rsp].start=func_addr[i].start;
 			addr[rsp].end=func_addr[i].end;
-		  printf("0x%08x call %s\trsp : %d \n",s->pc,addr[rsp].func_name,rsp);	
+		  //printf("0x%08x call %s\trsp : %d \n",s->pc,addr[rsp].func_name,rsp);	
 			log_write("0x%08x: call [%s @ 0x%08x]\n",s->pc,addr[rsp].func_name,s->dnpc);
 		  return ;
 		}
