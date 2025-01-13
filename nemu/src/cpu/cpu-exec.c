a9ac5d31592c0f414f095f836784a1452c6fa9f0
tracer-ysyx <tracer@ysyx.org>
2025-01-13 14:28:32 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:28:32 up 1 day, 13:19, 20 users,  load average: 0.13, 0.16, 0.14

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -207,7 +207,7 @@ static void execute(uint64_t n) {
   	
 Addr_Imfo *func_addr = read_sym_func(shdr_globle,sym_globle,str_globle,cnt_globle);
 for(int i=0;i<cnt_func_num;i++){
-printf("funcs is : %s start : %d end: %d\n",func_addr[i].func_name,func_addr[i].start,func_addr[i].end);
+printf("funcs is : %s start : %08x end: %08x\n",func_addr[i].func_name,func_addr[i].start,func_addr[i].end);
 }
   for (;n > 0; n --) {
 	char buf[512]={0};
