2a9e4925368cbfe81d8e861433baf850624c64e4
tracer-ysyx <tracer@ysyx.org>
2025-01-13 14:19:13 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:19:13 up 1 day, 13:10, 20 users,  load average: 0.36, 0.26, 0.16

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -158,12 +158,12 @@ printf("the first pc : %08x\n",s->pc);
 printf("the first dnpc : %08x\n",s->dnpc);
 
 if(s->pc==0x80000000){
+	printf("cnt_func_num=%d\n",cnt_func_num);
 		for(int i=0;i<cnt_func_num;i++){
 		if(func_addr[i].start<=s->pc&&func_addr[i].end>=s->pc){
 		addr[rsp].func_name=func_addr[i].func_name;	
 		//addr[rsp].start=func_addr[i].start;
 		//addr[rsp].end=func_addr[i].end;
-
 		printf("init is OK\n");
 		}	
 		}
