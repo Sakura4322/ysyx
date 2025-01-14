60273a0cdbb3171b9b910ecf054a00c9d5cf0d02
tracer-ysyx <tracer@ysyx.org>
2025-01-14 16:53:10 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:53:10 up 2 days, 0 min, 27 users,  load average: 0.30, 0.31, 0.22

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -7,11 +7,6 @@ AM_SRCS := riscv/npc/start.S \
            riscv/npc/trap.S \
            platform/dummy/vme.c \
            platform/dummy/mpe.c
-
-CFLAGS    += -fdata-sections -ffunction-sections
-LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
-						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
-LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
@@ -19,3 +14,7 @@ image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+run : image
+	$(MAKE) -C $(NPC_HOME) ISA=$(ISA) run IMG=$(IMAGE)
+
--- npc/csrc/verilog.cpp
+++ npc/csrc/verilog.cpp
@@ -2,12 +2,99 @@
 #include "verilated.h"
 #include <stdio.h>
 #include <verilated_vcd_c.h>  //启动波追踪
+#include <getopt.h> 
+#include <memory/paddr.h>
 //#include <nvboard.h>
 
 //接入nvboard
 //void nvboard_band_all_pins(Vverilog *top);
+uint32_t *vaddr;
+static long load_img() {
+  if (img_file == NULL) {
+    Log("No image is given. Use the default build-in image.");
+    return 4096; // built-in image size
+  }
 
+  FILE *fp = fopen(img_file, "rb");
+  Assert(fp, "Can not open '%s'", img_file);
 
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+
+  Log("The image is %s, size = %ld", img_file, size);
+	
+
+  vaddr =malloc(size);
+	Assert(vaddr,"Memory allocation faile\n");
+
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(vaddr, size, 1, fp);
+  assert(ret == 1);
+
+  fclose(fp);
+  return size;
+}
+
+static int parse_args(int argc, char *argv[]) {
+  //for(int i=0;i<100;i++){
+		
+	//printf("this is this : %s\n",*(argv+i));
+	//}
+	const struct option table[] = {
+    {"batch"    , no_argument      , NULL, 'b'},
+    {"log"      , required_argument, NULL, 'l'},
+    {"diff"     , required_argument, NULL, 'd'},
+    {"port"     , required_argument, NULL, 'p'},
+    {"help"     , no_argument      , NULL, 'h'},
+		//{"elf"      , required_argument, NULL, 'e'},
+    {0          , 0                , NULL,  0 },
+  };
+  int o;
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+    switch (o) {
+      case 'b': sdb_set_batch_mode(); break;
+      case 'p': sscanf(optarg, "%d", &difftest_port); break;
+      case 'l': log_file = optarg; break;
+      case 'd': diff_so_file = optarg; break;
+			//case 'e': if(optarg=="-h")
+      case 1:
+							/*
+							char temp[256]={0};
+							char *suffix;
+							strcpy(temp,optarg);
+							suffix=strchr(temp,'.');
+							suffix++;
+							img_file=(strcmp(suffix,"bin")==0)?optarg:img_file;
+							elf_file=(strcmp(suffix,"elf")==0)?optarg:elf_file;
+						  //printf("what is suffix : %s\n",suffix);
+						  //printf("what is temp : %s\n",temp);
+						  //printf("what is optarg : %s\n",optarg);
+						  printf("what is img_file : %s\n",img_file);
+						  printf("what is elf_file : %s\n",elf_file);
+							if(strcmp(suffix,"elf")==0){	
+              ehdr_globle=parse_elf(elf_file);
+						  shdr_globle=parse_shdr(ehdr_globle,elf_file);
+						  //find_shdr_type(ehdr_globle,shdr_globle);
+						  str_globle =parse_strtab(shdr_globle,elf_file);
+							sym_globle =parse_sym(shdr_globle,elf_file);
+							
+						//	printf("size of sym struct : %ld\n\n\n\n",sizeof(Elf32_Sym) );
+							*/
+								img_file = optarg; return 0;
+							break;
+
+      default:
+        printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+        printf("\t-b,--batch              run with batch mode\n");
+        printf("\t-l,--log=FILE           output log to FILE\n");
+        printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+        printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\n");
+        exit(0);
+    }
+  }
+  return 0;
+}
 
 int main(int argc,char **argv){
 	VerilatedContext* Contextp = new VerilatedContext;
@@ -28,6 +115,7 @@ int main(int argc,char **argv){
 	
 	std::srand(time(NULL));
 	int simTime=0;
+	/*
 	unsigned int inst[11] = {
     0xffc10113,  // addi sp, sp, -4
     0x00278713,  // addi a4, a5, 2
@@ -41,11 +129,15 @@ int main(int argc,char **argv){
     0x02010113,   // addi sp, sp, 32
     0x00100073 //ebreak
 };
+
+*/
+	parse_args(argc,argv);
+	long image_size=load_image();
 unsigned int clk=0;
 	while (!top->flag){
 		clk = clk ^ 1;
 		top->clk=clk;
-		if(clk) top->inst=inst[top->pc];
+		if(clk) top->inst=vaddr[top->pc];
 	Contextp->timeInc(1);
 	top ->eval();
 	 tfp->dump(Contextp->time());  // dump 波形数据
