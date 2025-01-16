fff880f25a24d384c757017e6357832114ca3fdc
tracer-ysyx <tracer@ysyx.org>
2025-01-16 11:22:35 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:22:35 up 2 days, 11:45, 28 users,  load average: 0.14, 0.15, 0.18

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -47,6 +47,36 @@ module ysyx_24090015_immI#(WIDTH=32) (
     );
 endmodule
 
+module ysyx_24090015_immU#(WIDTH=32) (
+	input [WIDTH-1 :0] inst_in,
+	output [WIDTH-1: 0] immU
+);
+	assign immU = {inst_in[31:12],12'b0};
+
+endmodule
+
+
+module ysyx_24090015_immJ#(WIDTH=32) (
+
+input [WIDTH-1 : 0] inst_in,
+output [WIDTH-1 : 0] immJ
+
+);
+
+	wire [19 : 0] init;
+	assign init={inst_in[31],{inst_in[19:12],{inst_in[20],inst_in[30:21]}}};
+	ysyx_24090015_SEXT#(
+		.DATA_WIDTH(20),
+		.WIDTH(32)		
+	) s1(
+			.in(init),
+			.out(immJ)
+		);
+
+	endmodule
+	
+	
+
 module ysyx_24090015_TYPE#(WIDTH=32) (
     input clk,
     input [WIDTH-1:0] inst,
@@ -58,16 +88,23 @@ module ysyx_24090015_TYPE#(WIDTH=32) (
         opcode = inst[6:0];
         case (opcode)
             7'b0010011: inst_type = `I; // I=2
+						7'b0010111: inst_type = `U;
+						7'b1101111: inst_type = `J;
+						7'b1100111: inst_type = `I;//jalr
+						7'b0000011: inst_type = `I;//lb,lh,lw,lbu,lhu
             default: inst_type = 0;
         endcase
     end
-endmodule 
+endmodule
+
+ 
 
 module ysyx_24090015_IDU#(WIDTH=32) (
     input clk,
     input [WIDTH-1:0] inst_in,
-    output  [WIDTH-1:0] imm,
-    output  ren1, ren2, wen,
+    output reg [WIDTH-1:0] imm,
+    output reg  ren1, ren2, wen,
+		output reg [4:0] rd,rs1,rs2
 );
 
     wire [2:0] inst_type;
@@ -79,18 +116,62 @@ module ysyx_24090015_IDU#(WIDTH=32) (
         .inst_type(inst_type)
     );
 
-    wire [WIDTH-1:0] temp_immI;
+    wire [WIDTH-1:0] temp_immI , temp_immU,temp_immJ;
     ysyx_24090015_immI#(
         .WIDTH(32)
     ) i0(
         .inst(inst_in),
-        .imm(temp_immI)
+        .immI(temp_immI)
     );
 
-		assign ren1=(inst_type==2);
-		assign ren2=0;
-		assign wen=(inst_type==2);
-		assign imm=temp_immI;
+		ysyx_24090015_immU#(
+		.WIDTH(32)	
+		) i1(
+			.inst_in(inst_in),
+			.immU(temp_immU)
+		);
+
+
+		ysyx_24090015_immJ#(
+		.WIDTH(32)	
+		) i2 (
+			.inst_in(inst_in),
+			.immJ(temp_immJ)
+		);
+
+
+		always @(*)begin
+				case(inst_type)
+					`I : begin 
+							rs1=inst_in[19:15];
+							rs2=0;
+							ren1=1;
+							ren2=0;
+							rd=inst_in[11:7];
+							wen=1;
+							imm=temp_immI;
+				end
+					`U : begin 
+							rs1=0;
+							rs2=0;
+							ren1=0;
+							ren2=0;
+							rd=inst_in[11:7];
+							wen=1;
+							imm=temp_immU;
+				end
+					`J : begin 
+							rs1=0;
+							rs2=0;
+							ren1=0;
+							ren2=0;
+							rd=inst_in[11:7];
+							wen=1;
+							imm=temp_immJ;
+				end
+				
+				endcase
+		end
 		
 endmodule
 
@@ -99,7 +180,6 @@ module ysyx_24090015_EXU#(WIDTH=32) (
     input [WIDTH-1:0] inst_in, imm,
     input [WIDTH-1:0] src1, src2,
     input [WIDTH-1:0] snpc,
-    output reg[4:0] rd,rs1,rs2,
     output reg [WIDTH-1:0] rd_wdata,
     output reg [WIDTH-1:0]  dnpc
 );
@@ -107,13 +187,33 @@ module ysyx_24090015_EXU#(WIDTH=32) (
     always @(*) begin
         dnpc = snpc;
         casez (inst_in)
-					32'b???????_?????_?????_000_?????_00100_11: begin //addi
-           rd  = inst_in[11:7];
-           rs1 = inst_in[19:15];
+					32'b???????_?????_?????_000_?????_00100_11: begin //addi I
            rd_wdata = src1 + imm;
             end
+					32'b???????_?????_?????_000_?????_11001_11: begin //jalr I
+
+					dnpc = ~((src1+imm)&{32{1'b1}});
+					rd_wdata = dnpc*4+4;
+
+					end
+					32'b???????_?????_?????_???_?????_00101_11: begin //auipc U
+						
+					rd_wdata = snpc*4 + imm;
+				
+					end
+					32'b???????_?????_?????_???_?????_01101_11: begin //lui U
+
+					rd_wdata = imm;
+
+					end
+					32'b???????_?????_?????_???_?????_11011_11: begin //jal J
+					
+					rd_wdata=snpc+4;
+					dnpc = dnpc+imm;
+
+				end
         endcase
-      
+        
     end
 endmodule
 
@@ -163,8 +263,11 @@ end
         .inst_in(inst),
         .imm(imm),
         .ren1(ren1),
+	.rs1(rs1),
         .ren2(ren2),
-        .wen(wen)
+	.rs2(rs2),
+        .wen(wen),
+	.rd(rd)
     );
 
     // EXU实例化
@@ -177,11 +280,7 @@ end
         .src1(src1),
         .src2(src2),
         .snpc(snpc),
-				.rd(rd),
-				.rs1(rs1),
-				.rs2(rs2),
         .rd_wdata(rd_wdata),
-       // .npc(pc),
         .dnpc(dnpc)
     );
 
