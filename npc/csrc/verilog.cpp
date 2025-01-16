07aa8f86d5c4c622b8feabf8ae9a5a5b5cc21ecd
tracer-ysyx <tracer@ysyx.org>
2025-01-16 13:28:44 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:28:44 up 2 days, 13:17, 28 users,  load average: 0.37, 0.62, 0.58

--- npc/csrc/verilog.cpp
+++ npc/csrc/verilog.cpp
@@ -149,9 +149,9 @@ int main(int argc,char **argv){
 	//parse_args(argc,argv);
 	long img_size=load_img();
   unsigned int clk=0;
-	int a=22;
-	//while (!top->flag){
-	while (a--){
+	int a=20;
+	while (!top->flag){
+	//while (a--){
 	
 		clk = clk ^ 1;
 		top->clk=clk;
