d35776dadd8d0489300ead2bbfe88275bce54eb3
tracer-ysyx <tracer@ysyx.org>
2025-01-16 10:44:29 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:44:29 up 2 days, 11:07, 28 users,  load average: 0.11, 0.37, 0.40

--- npc/csrc/verilog.cpp
+++ npc/csrc/verilog.cpp
@@ -124,7 +124,6 @@ int main(int argc,char **argv){
 	
 	std::srand(time(NULL));
 	int simTime=0;
-/*	
 	unsigned int inst[11] = {
     0xffc10113,  // addi sp, sp, -4
     0x00278713,  // addi a4, a5, 2
@@ -138,7 +137,6 @@ int main(int argc,char **argv){
     0x02010113,   // addi sp, sp, 32
     0x00100073 //ebreak
 };
-*/
    //getopt_lon(argc, argv, " ", table, NULL);
 	 argv++;
    img_file=*argv;
