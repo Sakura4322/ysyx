069257c34e07bb74640f65ce3dce94c2c7882291
tracer-ysyx <tracer@ysyx.org>
2025-01-17 15:58:02 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:58:02 up  2:45,  8 users,  load average: 1.07, 0.61, 0.57

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -236,6 +236,9 @@ if(((s->isa.inst.val & 0b00000000000000000000000001111111) == 0b0000000000000000
 	if(s->isa.inst.val == 0b00000000000000001000000001100111){
         for(int i = 0; i < cnt_func_num; i++){
             if(s->pc >= func_addr[i].start && s->pc < func_addr[i].end){
+							for(int i=0;i<rsp;i++){
+							printf(" ");	
+							}
                 printf("0x%08x ret %s\trsp : %d \n", s->pc, func_addr[i].func_name, rsp-1);	
 			          printf("addr[rsp] : %08x\t s->dnpc : %08x \n",addr[i],s->dnpc);
                 log_write("0x%08x: ret [%s @ 0x%08x]\n", s->pc, func_addr[i].func_name, s->dnpc);
@@ -252,6 +255,9 @@ if(((s->isa.inst.val & 0b00000000000000000000000001111111) == 0b0000000000000000
 		if(s->dnpc>=func_addr[i].start&&s->dnpc<func_addr[i].end){
 		  rsp++;
 			addr[rsp]=s->pc+4;
+							for(int i=0;i<rsp;i++){
+							printf(" ");	
+							}
 			printf("0x%08x call %s\trsp : %d \n",s->pc,func_addr[i].func_name,rsp);	
 			printf("addr[rsp] : %08x\t s->dnpc : %08x \n",addr[i],s->dnpc);
 		  //printf("ARE YOU OK??\n");
