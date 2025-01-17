ec3d239e3c2ad32e2c57d382f9832b7ec64d9d67
tracer-ysyx <tracer@ysyx.org>
2025-01-17 20:42:33 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:42:33 up  5:56, 14 users,  load average: 1.03, 0.96, 0.64

---
+++ npc/csrc/cpu_exec.c
@@ -0,0 +1,109 @@
+#include "common.h"
+#include "my_share.h"
+#include "cpu.h"
+
+NEMUState nemu_state;
+
+
+static void execute(uint64_t n) {
+  //Decode s;
+  /*
+	char iringbuf[20][128];
+	char iringbuf_reg_state[2][512];
+	int cout_pc_num=0;
+  */	
+
+
+  for (;n > 0; n --) {
+	//char buf[512]={0};
+    step_and_dump_wave();
+    //trace_and_difftest(&s, cpu.pc);
+
+
+
+/*itrace
+		for (int i=0;i<32;i++){//storage reg information
+		char buf_temp[16]={0};
+		sprintf(buf_temp,"%s : %08x\n",regs[i],(int)cpu.gpr[i]);	
+		strcat(buf,buf_temp);
+ 	}
+ 	
+ 	
+		strcpy(*(iringbuf_reg_state+cout_pc_num%2),buf);//use iringbuf storage the reg information
+	  iring_load(iringbuf,&s,cout_pc_num++);//storage the information of instructions		
+		*/
+
+/*ftrace
+#ifdef CONFIG_FTRACE
+    ftrace(func_addr,&s);
+#endif
+*/
+
+
+/*itrace
+		//printf("ARE YOU OK??\n");
+		if (is_exit_status_bad()&& nemu_state.state!=NEMU_RUNNING){
+			 cout_pc_num-=1;
+	for(int i=0;i<=cout_pc_num%20;i++){
+	if(i==cout_pc_num){
+
+ log_write("\n\n\n\nthe reg information : \n%s\n",*(iringbuf_reg_state+((cout_pc_num-1)%2)));
+ log_write("//////////////////////////////////////the wrong ///////////////////////////////////\n"); 		
+	}	
+ log_write("%s\n", *(iringbuf+i)); 
+	if(i==cout_pc_num){
+ log_write("//////////////////////////////////////the wrong ///////////////////////////////////\n"); 		
+ log_write("%s\n",*(iringbuf_reg_state+(cout_pc_num%2)));
+	}	
+	}	
+		}
+		
+*/
+
+    if (nemu_state.state != NEMU_RUNNING) break;
+    
+  }
+}
+
+void cpu_exec(uint64_t n) {
+  
+   switch (nemu_state.state) {
+    case NEMU_END: 
+    case NEMU_ABORT:
+      printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
+      return;
+    default: nemu_state.state = NEMU_RUNNING;
+  }
+
+  if (n == 0){
+  	nemu_state.state = NEMU_QUIT;
+   	}
+  execute(n);
+  
+
+  switch (nemu_state.state) {
+    case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
+
+    case NEMU_END: case NEMU_ABORT:
+    	nemu_state.halt=top->hit_good_or_bad;
+    	nemu_state.halt_pc=top->pc;
+      Log("nemu: %s at pc = " FMT_WORD,
+          (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
+           (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
+            ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
+          nemu_state.halt_pc);
+			//log_write()
+      // fall through
+    case NEMU_QUIT: 
+			nemu_state.state=NEMU_END;
+			return;
+      break;
+  }
+}
+
+
+/*
+void assert_fail_msg() {
+  isa_reg_display();
+}
+*/
--- npc/csrc/my_share.h
+++ npc/csrc/my_share.h
@@ -5,6 +5,6 @@ extern char *img_file;
 extern void sdb_mainloop();
 extern void step_and_dump_wave()
 extern void cpu_exec(uint64_t n);
-extern VerilatedContext *contextp = NULL;
-extern Vysyx_24090015_top* top=NULL;
-extern VerilatedVcdC *tfp=NULL;
+extern VerilatedContext *contextp;
+extern Vysyx_24090015_top* top;
+extern VerilatedVcdC *tfp;
