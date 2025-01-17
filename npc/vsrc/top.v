e13c6bca3a206d649a4a0e1fd58e426d74cbe03d
tracer-ysyx <tracer@ysyx.org>
2025-01-17 21:45:53 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:53 up  7:00, 14 users,  load average: 0.81, 0.82, 0.51

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -295,7 +295,7 @@ assign hit_good_or_bad=src1;
         .dnpc(dnpc)
     );
 wire [31:0]rf[31:0];
-import "DPI-C" function void get_reg_value(intput int a[32]);
+import "DPI-C" function void get_reg_value(input int a[32]);
     // 寄存器堆实例化
     ysyx_24090015_RegisterFile #(
         .ADDR_WIDTH(5),
