a4f7cfd4eacc7a1b341703eba5d5d046bdfae649
tracer-ysyx <tracer@ysyx.org>
2025-01-17 20:16:59 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:59 up  5:31, 14 users,  load average: 0.74, 0.47, 0.45

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -367,13 +367,12 @@ void cpu_exec(uint64_t n) {
   }
 
   uint64_t timer_start = get_time();
-
-  execute(n);
-  /*
-  if (n == 0){
+  
+if (n == 0){
   	nemu_state.state = NEMU_QUIT;
    	}
-*/
+  execute(n);
+ 
   uint64_t timer_end = get_time();
   g_timer += timer_end - timer_start;
 
