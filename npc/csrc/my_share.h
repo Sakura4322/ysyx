8eaba7dab03c58d1df1aa4b7047ce6a1fe923066
tracer-ysyx <tracer@ysyx.org>
2025-01-17 20:41:13 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:41:13 up  5:55, 14 users,  load average: 1.45, 0.96, 0.61

--- npc/csrc/my_share.h
+++ npc/csrc/my_share.h
@@ -3,7 +3,7 @@ extern long load_img();
 extern uint32_t *vaddr;
 extern char *img_file;
 extern void sdb_mainloop();
-extern step_and_dump_wave();
+extern void step_and_dump_wave()
 extern void cpu_exec(uint64_t n);
 extern VerilatedContext *contextp = NULL;
 extern Vysyx_24090015_top* top=NULL;
