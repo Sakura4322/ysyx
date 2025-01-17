b23cf79ba63e605d3bcc62fd0edd2bf39f6a2014
tracer-ysyx <tracer@ysyx.org>
2025-01-17 20:51:27 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:51:27 up  6:05, 14 users,  load average: 2.43, 1.69, 1.06

--- npc/csrc/debug.h
+++ npc/csrc/debug.h
@@ -1,24 +1,4 @@
-#define Log(format, ...) \
-    _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", \
-        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
-
-#define log_write(...) IFDEF(CONFIG_TARGET_NATIVE_ELF, \
-  do { \
-    extern FILE* log_fp; \
-    extern bool log_enable(); \
-    if (log_enable()) { \
-      fprintf(log_fp, __VA_ARGS__); \
-      fflush(log_fp); \
-    } \
-  } while (0) \
-)
-
-#define _Log(...) \
-  do { \
-    printf(__VA_ARGS__); \
-    log_write(__VA_ARGS__); \
-  } while (0)
-
+// ANSI 转义码
 #define ANSI_FG_BLACK   "\33[1;30m"
 #define ANSI_FG_RED     "\33[1;31m"
 #define ANSI_FG_GREEN   "\33[1;32m"
@@ -37,6 +17,32 @@
 #define ANSI_BG_WHITE   "\33[1;47m"
 #define ANSI_NONE       "\33[0m"
 
+// 格式化字符串
 #define ANSI_FMT(str, fmt) fmt str ANSI_NONE
 
+// 日志宏
+#define Log(format, ...) \
+    _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", \
+         __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 
+
+
+// 日志写入宏
+#define log_write(...) \
+    do { \
+        extern FILE* log_fp; \
+        extern bool log_enable(); \
+        if (log_enable()) { \
+            fprintf(log_fp, __VA_ARGS__); \
+            fflush(log_fp); \
+        } \
+    } while (0)
+
+// 内部日志宏
+#define _Log(...) \
+    do { \
+        printf(__VA_ARGS__); \
+        log_write(__VA_ARGS__); \
+    } while (0)
+
+#endif  // DEBUG_H
