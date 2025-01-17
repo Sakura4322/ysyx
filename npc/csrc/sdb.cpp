57a14efc55a9fc616a184310606e91bea0807609
tracer-ysyx <tracer@ysyx.org>
2025-01-17 21:04:33 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:04:33 up  6:18, 14 users,  load average: 0.27, 0.65, 0.78

--- npc/csrc/sdb.cpp
+++ npc/csrc/sdb.cpp
@@ -39,7 +39,7 @@ cpu_exec(0);
 static int cmd_help(char *args);
 
 
-/*
+
 static int cmd_si(char *args) {  
   static  int n = 1; // 默认单步执行1条指令  
 char *arg = strtok(NULL , " ");
@@ -52,7 +52,6 @@ char *arg = strtok(NULL , " ");
 
     return 0; // 返回0表示执行成功  
 }  
-*/
 
 /*
 static int cmd_info(char *args){
@@ -166,7 +165,7 @@ static struct {
   { "q", "Exit NEMU", cmd_q },
 
   /* TODO: Add more commands */
-//{"si","让程序单步执行N条指令后暂停执行,当N没有给出时, 缺省为1",cmd_si},
+{"si","让程序单步执行N条指令后暂停执行,当N没有给出时, 缺省为1",cmd_si},
 //{"info","打印寄存器状态,打印监视点信息",cmd_info},
 //{"x","求出表达式EXPR的值, 将结果作为起始内存地址, 以十六进制形式输出连续的N个4字节",cmd_x},
 //{"p","求出表达式EXPR的值, EXPR支持的运算请见调试中的表达式求值小节",cmd_p},
