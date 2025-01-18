c0f2ae81937b0900c0dfe84c3858d6257add0aed
tracer-ysyx <tracer@ysyx.org>
2025-01-18 15:03:52 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:03:52 up 11:17, 14 users,  load average: 0.27, 0.20, 0.22

--- npc/csrc/DPI.cpp
+++ npc/csrc/DPI.cpp
@@ -32,6 +32,7 @@ void read_regs() {
 	int wen=read_wire(1);
 	int waddr=read_wire(2);
 	int wdata=read_wire(3);
+	printf("wen: %d,waddr : %d,wdata : %d\n",wen,waddr,wdata);
 			if(wen){
 			reg_value[waddr]=wdata;	
 			}
