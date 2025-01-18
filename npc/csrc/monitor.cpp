90b8bf8b19be414ee06c27c3090099105aa8bc25
tracer-ysyx <tracer@ysyx.org>
2025-01-18 20:42:31 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:42:31 up 14:59, 18 users,  load average: 0.10, 0.11, 0.13

--- npc/csrc/monitor.cpp
+++ npc/csrc/monitor.cpp
@@ -58,6 +58,7 @@ int parse_args(int argc, char *argv[]) {
   int o;
   while ( (o = getopt_long(argc, argv, "-bhl:", table, NULL)) != -1) {
   //while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
+	printf("o is : %d\n",o);
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       //case 'p': sscanf(optarg, "%d", &difftest_port); break;
