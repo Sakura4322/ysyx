3c079dd736fc922d76217745593925a0c74c8835
tracer-ysyx <tracer@ysyx.org>
2025-01-18 13:57:08 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:57:08 up 10:10, 14 users,  load average: 0.07, 0.20, 0.23

--- npc/csrc/DPI.cpp
+++ npc/csrc/DPI.cpp
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "svdpi.h"
 #include "my_share.h"
+#include "verilated_dpi.h"
 
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
@@ -21,8 +22,8 @@ extern "C" int ebreak(int a){
 int reg_value[32]={0};
 
 extern "C" int read_wire(int sec);
-svScope scope = svGetScopeFromName("TOP.top");
-svSetScope(scope);
+	svScope scope = svGetScopeFromName("TOP.top");
+	svSetScope(scope);
 void read_regs() {
 	int wen=read_wire(1);
 	int waddr=read_wire(2);
