ebd14d94aba1ee7aaaf40bda2412dfad9d9de1a4
tracer-ysyx <tracer@ysyx.org>
2025-01-18 14:28:02 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:28:02 up 10:41, 14 users,  load average: 0.11, 0.17, 0.17

--- npc/obj_dir/Vysyx_24090015_top.mk
+++ npc/obj_dir/Vysyx_24090015_top.mk
@@ -38,7 +38,7 @@ VM_USER_CFLAGS = \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
-	-lreadline -l/verilator/include/vltstd \
+	-lreadline -l//home/sakura/verilator//verilator/include/vltstd \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
