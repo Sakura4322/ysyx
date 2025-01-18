00a73c08a2bacb92240e5d8dd027de4facd4a93a
tracer-ysyx <tracer@ysyx.org>
2025-01-18 20:29:22 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:29:22 up 14:46, 17 users,  load average: 0.15, 0.17, 0.11

--- npc/csrc/verilog.cpp
+++ npc/csrc/verilog.cpp
@@ -97,7 +97,7 @@ int main(int argc,char **argv){
    img_file=*argv;
 	 printf("\n\n\n\n\n\n\nimg_file : %s\n\n\n\n\n",img_file);
 */
-	printf("I'M OK NOW\n"); 
+	printf("argv : %s\n",argv); 
 	parse_args(argc,argv);
 	 printf("\n\n\n\n\n\n\nimg_file : %s\n\n\n\n\n",img_file);
 	long img_size=load_img();
