66adbe3100fd42eaff21510e93ec4129b3c63a8a
tracer-ysyx <tracer@ysyx.org>
2025-01-19 11:38:30 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:38:30 up 17:56, 19 users,  load average: 0.46, 0.38, 0.44

--- npc/csrc/DPI.cpp
+++ npc/csrc/DPI.cpp
@@ -4,12 +4,6 @@
 #include "verilated_dpi.h"
 #include "Vysyx_24090015_top__Dpi.h"
 
-const char *regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
 
 extern "C" int ebreak(int a){
 		//printf("the input ebreak inst is : %08x\n",a);
