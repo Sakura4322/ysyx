fd997dbd2241c1caf562b4a0f017aaeb49793a5c
tracer-ysyx <tracer@ysyx.org>
2025-01-19 14:14:11 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:14:11 up 18:56, 19 users,  load average: 0.61, 0.50, 0.46

--- npc/csrc/disasm.h
+++ npc/csrc/disasm.h
@@ -1,6 +1,8 @@
 #ifndef DISASM_H
 #define DISASM_H
 
+#include <stdint.h>
+
 extern "C" {
     void init_disasm(const char *triple);
     void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
