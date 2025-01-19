3f25cdfdb7d8de0ac599a1e88148c7238cb4164c
tracer-ysyx <tracer@ysyx.org>
2025-01-19 20:46:13 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:46:13 up 22:39, 23 users,  load average: 0.17, 0.20, 0.23

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -27,7 +27,7 @@ $(BINARY):: compile_git
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
 #override ARGS += --elf=$(ELF)
-#override ARGS += -b
+override ARGS += -b
 
 # Command to execute NEMU
 IMG ?=
