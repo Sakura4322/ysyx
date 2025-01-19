8bbff8d2829f018281b8431156caf041d67798ed
tracer-ysyx <tracer@ysyx.org>
2025-01-19 13:57:25 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:57:25 up 18:39, 19 users,  load average: 0.28, 0.23, 0.29

--- npc/csrc/DPI.cpp
+++ npc/csrc/DPI.cpp
@@ -12,7 +12,13 @@ extern "C" int ebreak(int a){
 		}	
 		else return 0;
 }
-//size of sram
+
+const char *regs[] = {                                                
+		"$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",                     
+		"s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",                     
+		"a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",                     
+		"s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"                    
+}; 
 
 uint32_t reg_value[32]={0};//Register file
 													 
--- npc/csrc/my_share.h
+++ npc/csrc/my_share.h
@@ -8,14 +8,15 @@ extern VerilatedContext *contextp;
 extern Vysyx_24090015_top* top;
 extern VerilatedVcdC *tfp;
 
-
+/*
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
 	"a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
 	"s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6" 
 };
-
+*/
+extern char *regs[];
 extern uint32_t reg_value[32];
 extern uint32_t sram[MAX_SRAM_SIZE];
 
