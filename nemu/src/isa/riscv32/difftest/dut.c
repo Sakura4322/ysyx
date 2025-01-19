30d9b67460d7c414f775eae809e979f06790b2e3
tracer-ysyx <tracer@ysyx.org>
2025-01-19 20:35:26 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:35:26 up 22:28, 23 users,  load average: 0.17, 0.11, 0.20

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,13 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+	for(int i=0;i<32;i++){
+	if(ref_r->gpr[i]!=cpu.gpr[i]){
+		printf("WORING IN PC = 0x%08x\n",pc);
+	return false;
+	}	
+	}
+  return true;
 }
 
 void isa_difftest_attach() {
