be7da719515e666233c7f623ece0d96cb029f9f7
tracer-ysyx <tracer@ysyx.org>
2025-01-19 21:51:54 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:51:54 up 23:45, 23 users,  load average: 0.52, 0.22, 0.20

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -354,7 +354,6 @@ void assert_fail_msg() {
   isa_reg_display();
   statistic();
 }
-
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
   g_print_step = (n < MAX_INST_TO_PRINT);
@@ -380,6 +379,17 @@ if (n == 0){
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
     case NEMU_END: case NEMU_ABORT:
+#ifdef CONFIG_FTRACE
+	if(func_addr!=NULL)
+free(func_addr);	
+	if(shdr_globle!=NULL)
+free(shdr_globle);
+	if(sym_globle!=NULL)
+free(sym_globle);
+	if(str_globle!=NULL)
+free(str_globle);
+
+#endif
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
