1fd0cd78604846c2a4ad4ba6ca75301fef0f7fc7
tracer-ysyx <tracer@ysyx.org>
2025-01-20 16:53:25 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:53:25 up  3:27, 12 users,  load average: 1.29, 0.60, 0.44

---
+++ npc/obj_dir/Vysyx_24090015_top___024root.h
@@ -0,0 +1,65 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design internal header
+// See Vysyx_24090015_top.h for the primary calling header
+
+#ifndef VERILATED_VYSYX_24090015_TOP___024ROOT_H_
+#define VERILATED_VYSYX_24090015_TOP___024ROOT_H_  // guard
+
+#include "verilated.h"
+
+class Vysyx_24090015_top__Syms;
+class Vysyx_24090015_top___024unit;
+
+
+class Vysyx_24090015_top___024root final : public VerilatedModule {
+  public:
+    // CELLS
+    Vysyx_24090015_top___024unit* __PVT____024unit;
+
+    // DESIGN SPECIFIC STATE
+    VL_IN8(clk,0,0);
+    VL_OUT8(flag,0,0);
+    VL_OUT8(hit_good_or_bad,0,0);
+    CData/*4:0*/ ysyx_24090015_top__DOT__rd;
+    CData/*4:0*/ ysyx_24090015_top__DOT__rs1;
+    CData/*4:0*/ ysyx_24090015_top__DOT__rs2;
+    CData/*0:0*/ ysyx_24090015_top__DOT__ren1;
+    CData/*0:0*/ ysyx_24090015_top__DOT__ren2;
+    CData/*0:0*/ ysyx_24090015_top__DOT__wen;
+    CData/*2:0*/ ysyx_24090015_top__DOT__idu0__DOT__inst_type;
+    CData/*6:0*/ ysyx_24090015_top__DOT__idu0__DOT__t0__DOT__opcode;
+    CData/*0:0*/ ysyx_24090015_top__DOT__idu0__DOT__i0__DOT__clk;
+    CData/*0:0*/ __Vtrigrprev__TOP__clk;
+    CData/*0:0*/ __VactContinue;
+    VL_IN(inst,31,0);
+    VL_OUT(pc,31,0);
+    VL_OUT(dnpc,31,0);
+    IData/*31:0*/ ysyx_24090015_top__DOT__ebreak_ret;
+    IData/*31:0*/ ysyx_24090015_top__DOT__imm;
+    IData/*31:0*/ ysyx_24090015_top__DOT__src1;
+    IData/*31:0*/ ysyx_24090015_top__DOT__rd_wdata;
+    IData/*31:0*/ __Vfunc_ebreak__1__Vfuncout;
+    IData/*31:0*/ __VstlIterCount;
+    IData/*31:0*/ __VicoIterCount;
+    IData/*31:0*/ __VactIterCount;
+    VlUnpacked<IData/*31:0*/, 32> ysyx_24090015_top__DOT__reg0__DOT__rf;
+    VlUnpacked<CData/*0:0*/, 3> __Vm_traceActivity;
+    VlTriggerVec<1> __VstlTriggered;
+    VlTriggerVec<1> __VicoTriggered;
+    VlTriggerVec<1> __VactTriggered;
+    VlTriggerVec<1> __VnbaTriggered;
+
+    // INTERNAL VARIABLES
+    Vysyx_24090015_top__Syms* const vlSymsp;
+
+    // CONSTRUCTORS
+    Vysyx_24090015_top___024root(Vysyx_24090015_top__Syms* symsp, const char* v__name);
+    ~Vysyx_24090015_top___024root();
+    VL_UNCOPYABLE(Vysyx_24090015_top___024root);
+
+    // INTERNAL METHODS
+    void __Vconfigure(bool first);
+} VL_ATTR_ALIGNED(VL_CACHE_LINE_BYTES);
+
+
+#endif  // guard
---
+++ npc/obj_dir/Vysyx_24090015_top___024unit__DepSet_h33d480f6__0.cpp
@@ -0,0 +1,21 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vysyx_24090015_top.h for the primary calling header
+
+#include "verilated.h"
+#include "verilated_dpi.h"
+
+#include "Vysyx_24090015_top__Syms.h"
+#include "Vysyx_24090015_top___024unit.h"
+
+extern "C" int ebreak(int a);
+
+VL_INLINE_OPT void Vysyx_24090015_top___024unit____Vdpiimwrap_ebreak_TOP____024unit(IData/*31:0*/ a, IData/*31:0*/ &ebreak__Vfuncrtn) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+        Vysyx_24090015_top___024unit____Vdpiimwrap_ebreak_TOP____024unit\n"); );
+    // Body
+    int a__Vcvt;
+    for (size_t a__Vidx = 0; a__Vidx < 1; ++a__Vidx) a__Vcvt = a;
+    int ebreak__Vfuncrtn__Vcvt;
+    ebreak__Vfuncrtn__Vcvt = ebreak(a__Vcvt);
+    ebreak__Vfuncrtn = ebreak__Vfuncrtn__Vcvt;
+}
