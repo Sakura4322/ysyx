a4c1bf0f3adfe3a4d4315bfdc9e732f8f6994e70
tracer-ysyx <tracer@ysyx.org>
2025-01-20 20:11:41 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:11:41 up  6:02, 14 users,  load average: 0.11, 0.14, 0.17

--- npc/obj_dir/Vysyx_24090015_top.mk
+++ npc/obj_dir/Vysyx_24090015_top.mk
@@ -38,7 +38,7 @@ VM_USER_CFLAGS = \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
-	-lreadline -lLLVM-14 /home/sakura/ysyx-workbench/nemu/build/riscv32-nemu-interpreter -Wl,-rpath=/home/sakura/ysyx-workbench/nemu/build/ \
+	-lreadline -lLLVM-14 /home/sakura/ysyx-workbench/nemu/build/riscv32-nemu-interpreter.so -Wl,-rpath=/home/sakura/ysyx-workbench/nemu/build/ \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
