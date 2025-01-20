094dc63f42fe47705a13a2dda3d7114d2a302865
tracer-ysyx <tracer@ysyx.org>
2025-01-20 15:07:26 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:07:26 up  1:41, 11 users,  load average: 0.62, 0.28, 0.36

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -12,6 +12,8 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
+LIBS_SO = $(NEMU_HOME)/build/riscv32-nemu-interpreter-so
+LDFLAGS += -L$(LIBS_SO)
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
