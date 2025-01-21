00c129899e5ecfe0aed954420dba8ac3c904c673
tracer-ysyx <tracer@ysyx.org>
2025-01-21 14:38:36 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:38:36 up  9:15, 24 users,  load average: 0.52, 0.55, 0.57

--- npc/csrc/monitor.cpp
+++ npc/csrc/monitor.cpp
@@ -8,7 +8,7 @@ char *log_file=NULL;
 char *elf_file=NULL;
 FILE *log_fp=NULL;
 char *diff_so_file =NULL;
-static int diff_port=6666;
+static int difftest_port=6666;
 
 long load_img() {
   if (img_file == NULL) {
