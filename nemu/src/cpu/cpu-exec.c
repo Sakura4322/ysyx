0d53aeb7590251df6ad380cec01708ada6d44812
tracer-ysyx <tracer@ysyx.org>
2025-01-21 18:34:05 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:34:05 up 13:10, 25 users,  load average: 0.87, 0.51, 0.50

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -60,6 +60,7 @@ printf("exec_once : coming exec_once\n");
   s->pc = pc;
   s->snpc = pc;
   isa_exec_once(s);
+  printf("s->snpc : %08x",s->snpc);
   cpu.pc = s->dnpc;
   printf("cpu.pc: 0x%08x\n",cpu.pc);
 #ifdef CONFIG_ITRACE
