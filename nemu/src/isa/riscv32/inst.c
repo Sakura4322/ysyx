662974d686154c12e89955d6bc5be7c7fb20952e
tracer-ysyx <tracer@ysyx.org>
2025-01-21 18:39:16 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:39:16 up 13:15, 25 users,  load average: 0.31, 0.39, 0.44

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -67,7 +67,7 @@ static int decode_exec(Decode *s) {
   word_t src1 = 0, src2 = 0, imm = 0;
   printf("s->snpc :%08x\n",s->snpc);
   s->dnpc = s->snpc;
-  printf("s->snpc :%08x\n",s->dnpc);
+  printf("s->dnpc :%08x\n",s->dnpc);
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
     decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
@@ -135,7 +135,7 @@ INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
 INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
-
+printf("s->dnpc :%08x\n",s->dnpc);
   return 0;
 }
 
