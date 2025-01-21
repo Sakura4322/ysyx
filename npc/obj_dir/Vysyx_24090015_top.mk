3ea614e3f09b09528ff805fc80d959d704cf9a75
tracer-ysyx <tracer@ysyx.org>
2025-01-21 14:41:18 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:41:18 up  9:17, 24 users,  load average: 0.24, 0.42, 0.52

--- npc/obj_dir/Vysyx_24090015_top.mk
+++ npc/obj_dir/Vysyx_24090015_top.mk
@@ -44,6 +44,7 @@ VM_USER_LDLIBS = \
 VM_USER_CLASSES = \
 	DPI \
 	cpu_exec \
+	difftest \
 	disasm \
 	monitor \
 	sdb \
@@ -66,6 +67,8 @@ VPATH += $(VM_USER_DIR)
 DPI.o: /home/sakura/ysyx-workbench/npc/csrc/DPI.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 cpu_exec.o: /home/sakura/ysyx-workbench/npc/csrc/cpu_exec.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+difftest.o: /home/sakura/ysyx-workbench/npc/csrc/difftest.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 disasm.o: /home/sakura/ysyx-workbench/npc/csrc/disasm.cc
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
