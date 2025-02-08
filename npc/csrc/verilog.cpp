7157f6b44d748ff442e28d9a6ba05d1acae92df7
tracer-ysyx <tracer@ysyx.org>
2025-02-08 21:11:33 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:11:33 up 1 day, 27 min, 25 users,  load average: 0.95, 1.22, 1.32

--- npc/csrc/verilog.cpp
+++ npc/csrc/verilog.cpp
@@ -40,7 +40,7 @@ contextp->traceEverOn(true);
 tfp = new VerilatedVcdC;
 top->trace(tfp,99); 
 tfp->open("wave.vcd");
-cpu->pc=0x80000000;
+cpu.pc=0x80000000;
 std::srand(time(NULL));
 init_disasm("riscv32");			//init disasm 
 parse_args(argc,argv);			//parse args
