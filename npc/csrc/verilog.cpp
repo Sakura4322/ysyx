032383b200ca0fa3e49b4a3e8f13e4db003a20ed
tracer-ysyx <tracer@ysyx.org>
2025-02-11 19:31:57 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:31:57 up  8:40,  5 users,  load average: 1.11, 1.10, 0.85

--- npc/csrc/verilog.cpp
+++ npc/csrc/verilog.cpp
@@ -49,8 +49,9 @@ long img_size=load_img();
 init_difftest(diff_so_file, img_size, difftest_port);	//init difftest port
 
 printf("Inst\n");
-for (int i=0;i<img_size/4;i++){
-	printf("%08x : %08x\n",0x80000000+i*4,pmem_read(0x80000000+i*4));
+for (int i=0;i<img_size;i++){
+	//printf("%08x : %08x\n",0x80000000+i*4,pmem_read(0x80000000+i*4));
+	printf("%x\n".vaddr[i]);
 }
 }
 
