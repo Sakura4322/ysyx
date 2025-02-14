c82fa1ba150d1244eb91d2bcea95785c1372a7e4
tracer-ysyx <tracer@ysyx.org>
2025-02-14 12:23:28 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:23:28 up 20:26,  5 users,  load average: 2.36, 1.36, 0.90

--- npc/csrc/DPI.cpp
+++ npc/csrc/DPI.cpp
@@ -54,9 +54,9 @@ extern "C" int pmem_read(int raddr){
 //printf("vaddr : %08x\n",raddr-CONFIG_MBASE);
 if(raddr<0x80000000||raddr>0x8fffffff){
 	printf("address = " %08x " is out of bound of pmem [" 0x80000000 ", " 0x8fffffff "] at pc = " %08x,
-      addr, cpu.pc);
+      raddr, cpu.pc);
 	  log_write("address = " %08x " is out of bound of pmem [" 0x80000000 ", " 0x8fffffff "] at pc = " %08x,
-      addr, cpu.pc);
+      raddr, cpu.pc);
 	exit(-1);
 }
 log_write("pmem_read\taddr : %08x\tdata : %08x\n",raddr,*(uint32_t *)(vaddr + (raddr -CONFIG_MBASE)));
@@ -69,12 +69,12 @@ extern "C" void pmem_write(int waddr,int wdata,char wmask){
 	uint8_t *temp= (uint8_t *)&wdata;
 	if(raddr<0x80000000||raddr>0x8fffffff){
 		printf("address = " %08x " is out of bound of pmem [" 0x80000000 ", " 0x8fffffff "] at pc = " %08x,
-		  addr, cpu.pc);
+		  waddr, cpu.pc);
 		  log_write("address = " %08x " is out of bound of pmem [" 0x80000000 ", " 0x8fffffff "] at pc = " %08x,
-		  addr, cpu.pc);
+		  waddr, cpu.pc);
 		exit(-1);
 	}
-	
+
 	if(wmask==0b0001){
 
 		log_write("pmem_write\taddr : %08x\tdata : %02x\n",waddr,temp[0]);
