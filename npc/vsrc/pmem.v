88d22a0dd64fee9fccf6628c9ac8ebc3fb326faa
tracer-ysyx <tracer@ysyx.org>
2025-02-14 12:51:47 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:51:47 up 20:54,  5 users,  load average: 0.79, 0.87, 0.85

--- npc/vsrc/pmem.v
+++ npc/vsrc/pmem.v
@@ -4,7 +4,7 @@ import "DPI-C" function void pmem_write(
 
 module ysyx_24090015_pmem#(WIDTH = 32) (
 
-	input  valid ,wen,
+	input  clk,valid ,wen,
   input [7:0] wmask,
 	input [WIDTH-1:0] raddr,waddr,wdata,
 	output [WIDTH-1:0] rdata
