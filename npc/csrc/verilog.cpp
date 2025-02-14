94abd1de30a58e518b359518bfc58366a2c282bf
tracer-ysyx <tracer@ysyx.org>
2025-02-14 12:02:04 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:02:04 up 20:05,  5 users,  load average: 1.25, 1.03, 0.87

--- npc/csrc/verilog.cpp
+++ npc/csrc/verilog.cpp
@@ -63,8 +63,9 @@ void step_and_dump_wave(Decode *s){
 	
 	clk = clk ^ 1;
 	top->clk=clk;
-	top ->eval();
 	printf("OK!\n");
+	top ->eval();
+	
 //	printf("top->pc : 0x%08x\n",top->pc);
 //	printf("vaddr index: %d\n",top->pc/4);
 
