84cd9f96fb1f46b837db6ca560c03874ae56953e
tracer-ysyx <tracer@ysyx.org>
2025-02-17 22:32:28 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:32:28 up 19:08,  6 users,  load average: 1.44, 1.45, 1.43

--- npc/obj_dir/Vysyx_24090015_top___024root__DepSet_h4c88153f__0.cpp
+++ npc/obj_dir/Vysyx_24090015_top___024root__DepSet_h4c88153f__0.cpp
@@ -670,19 +670,18 @@ VL_INLINE_OPT void Vysyx_24090015_top___024root___ico_sequent__TOP__0(Vysyx_2409
             (vlSelf->ysyx_24090015_top__DOT__src1 < vlSelf->ysyx_24090015_top__DOT__src2);
     } else if ((0x3033U == (0xfe00707fU & vlSelf->inst))) {
         vlSelf->ysyx_24090015_top__DOT__rd_wdata = 
-            ((1U & (((((~ (vlSelf->ysyx_24090015_top__DOT__src1 
-                           >> 0x1fU)) & (~ (vlSelf->ysyx_24090015_top__DOT__src2 
-                                            >> 0x1fU))) 
-                      & (vlSelf->ysyx_24090015_top__DOT__src1 
-                         < vlSelf->ysyx_24090015_top__DOT__src2)) 
-                     | (((vlSelf->ysyx_24090015_top__DOT__src1 
-                          & vlSelf->ysyx_24090015_top__DOT__src2) 
-                         >> 0x1fU) & (vlSelf->ysyx_24090015_top__DOT__src1 
-                                      < vlSelf->ysyx_24090015_top__DOT__src2))) 
-                    | ((vlSelf->ysyx_24090015_top__DOT__src1 
-                        >> 0x1fU) & (~ (vlSelf->ysyx_24090015_top__DOT__src2 
-                                        >> 0x1fU)))))
-              ? 1U : 0U);
+            (1U & (((((~ (vlSelf->ysyx_24090015_top__DOT__src1 
+                          >> 0x1fU)) & (~ (vlSelf->ysyx_24090015_top__DOT__src2 
+                                           >> 0x1fU))) 
+                     & (vlSelf->ysyx_24090015_top__DOT__src1 
+                        < vlSelf->ysyx_24090015_top__DOT__src2)) 
+                    | (((vlSelf->ysyx_24090015_top__DOT__src1 
+                         & vlSelf->ysyx_24090015_top__DOT__src2) 
+                        >> 0x1fU) & (vlSelf->ysyx_24090015_top__DOT__src1 
+                                     < vlSelf->ysyx_24090015_top__DOT__src2))) 
+                   | ((vlSelf->ysyx_24090015_top__DOT__src1 
+                       >> 0x1fU) & (~ (vlSelf->ysyx_24090015_top__DOT__src2 
+                                       >> 0x1fU)))));
     }
 }
 
@@ -1049,18 +1048,17 @@ VL_INLINE_OPT void Vysyx_24090015_top___024root___nba_comb__TOP__0(Vysyx_2409001
             (vlSelf->ysyx_24090015_top__DOT__src1 < vlSelf->ysyx_24090015_top__DOT__src2);
     } else if ((0x3033U == (0xfe00707fU & vlSelf->inst))) {
         vlSelf->ysyx_24090015_top__DOT__rd_wdata = 
-            ((1U & (((((~ (vlSelf->ysyx_24090015_top__DOT__src1 
-                           >> 0x1fU)) & (~ (vlSelf->ysyx_24090015_top__DOT__src2 
-                                            >> 0x1fU))) 
-                      & (vlSelf->ysyx_24090015_top__DOT__src1 
-                         < vlSelf->ysyx_24090015_top__DOT__src2)) 
-                     | (((vlSelf->ysyx_24090015_top__DOT__src1 
-                          & vlSelf->ysyx_24090015_top__DOT__src2) 
-                         >> 0x1fU) & (vlSelf->ysyx_24090015_top__DOT__src1 
-                                      < vlSelf->ysyx_24090015_top__DOT__src2))) 
-                    | ((vlSelf->ysyx_24090015_top__DOT__src1 
-                        >> 0x1fU) & (~ (vlSelf->ysyx_24090015_top__DOT__src2 
-                                        >> 0x1fU)))))
-              ? 1U : 0U);
+            (1U & (((((~ (vlSelf->ysyx_24090015_top__DOT__src1 
+                          >> 0x1fU)) & (~ (vlSelf->ysyx_24090015_top__DOT__src2 
+                                           >> 0x1fU))) 
+                     & (vlSelf->ysyx_24090015_top__DOT__src1 
+                        < vlSelf->ysyx_24090015_top__DOT__src2)) 
+                    | (((vlSelf->ysyx_24090015_top__DOT__src1 
+                         & vlSelf->ysyx_24090015_top__DOT__src2) 
+                        >> 0x1fU) & (vlSelf->ysyx_24090015_top__DOT__src1 
+                                     < vlSelf->ysyx_24090015_top__DOT__src2))) 
+                   | ((vlSelf->ysyx_24090015_top__DOT__src1 
+                       >> 0x1fU) & (~ (vlSelf->ysyx_24090015_top__DOT__src2 
+                                       >> 0x1fU)))));
     }
 }
