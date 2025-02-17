546f3c6dae313c648b15b6f9539656c31d01fed6
tracer-ysyx <tracer@ysyx.org>
2025-02-17 17:16:03 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:16:03 up 13:51,  6 users,  load average: 3.05, 2.25, 1.75

--- npc/csrc/difftest_dut.cpp
+++ npc/csrc/difftest_dut.cpp
@@ -39,16 +39,13 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 
 static void checkregs(CPU_state *ref, uint32_t pc) {
   for(int i=0;i<32;i++){
-    if(ref->pc!=cpu.pc){
-
-    }
   	if (ref->gpr[i]!=cpu.gpr[i]||ref->pc!=cpu.pc) {
     npc_state.state = NPC_ABORT;
     npc_state.halt_pc = pc;
     npc_reg_display();
     if(ref->pc!=cpu.pc){
       printf("%s\tdiffrent : pc\n",s.logbuf);
-    }else{
+    }else if(ref->gpr[i]!=cpu.gpr[i]){
     printf("%s\tdiffrent : %s\n",s.logbuf,regs[i]);
     }
     printf("ref_regs info\n");
