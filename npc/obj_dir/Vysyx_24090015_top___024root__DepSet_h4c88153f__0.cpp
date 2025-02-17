0a35358c0e207bafd0e14cea21a78c17bd39aac6
tracer-ysyx <tracer@ysyx.org>
2025-02-17 23:16:38 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:16:38 up 19:52,  6 users,  load average: 1.73, 1.85, 1.58

--- npc/obj_dir/Vysyx_24090015_top___024root__DepSet_h4c88153f__0.cpp
+++ npc/obj_dir/Vysyx_24090015_top___024root__DepSet_h4c88153f__0.cpp
@@ -589,12 +589,29 @@ VL_INLINE_OPT void Vysyx_24090015_top___024root___ico_sequent__TOP__0(Vysyx_2409
                                  < vlSelf->ysyx_24090015_top__DOT__imm)
                               : ((0x40005013U == (0xfe00707fU 
                                                   & vlSelf->inst))
-                                  ? ((0x80000000U & vlSelf->ysyx_24090015_top__DOT__src1) 
-                                     | (0x7fffffffU 
-                                        & ((0x7fffffffU 
-                                            & vlSelf->ysyx_24090015_top__DOT__src1) 
-                                           >> (0x1fU 
-                                               & vlSelf->ysyx_24090015_top__DOT__imm))))
+                                  ? ((vlSelf->ysyx_24090015_top__DOT__src1 
+                                      >> 0x1fU) ? (
+                                                   ((0x1fU 
+                                                     >= 
+                                                     ((IData)(0x1fU) 
+                                                      - 
+                                                      (0x1fU 
+                                                       & vlSelf->ysyx_24090015_top__DOT__imm)))
+                                                     ? 
+                                                    ((IData)(0xffffffffU) 
+                                                     << 
+                                                     ((IData)(0x1fU) 
+                                                      - 
+                                                      (0x1fU 
+                                                       & vlSelf->ysyx_24090015_top__DOT__imm)))
+                                                     : 0U) 
+                                                   | (vlSelf->ysyx_24090015_top__DOT__src1 
+                                                      >> 
+                                                      (0x1fU 
+                                                       & vlSelf->ysyx_24090015_top__DOT__imm)))
+                                      : (vlSelf->ysyx_24090015_top__DOT__src1 
+                                         >> (0x1fU 
+                                             & vlSelf->ysyx_24090015_top__DOT__imm)))
                                   : ((0x4013U == (0x707fU 
                                                   & vlSelf->inst))
                                       ? (vlSelf->ysyx_24090015_top__DOT__src1 
@@ -967,12 +984,29 @@ VL_INLINE_OPT void Vysyx_24090015_top___024root___nba_comb__TOP__0(Vysyx_2409001
                                  < vlSelf->ysyx_24090015_top__DOT__imm)
                               : ((0x40005013U == (0xfe00707fU 
                                                   & vlSelf->inst))
-                                  ? ((0x80000000U & vlSelf->ysyx_24090015_top__DOT__src1) 
-                                     | (0x7fffffffU 
-                                        & ((0x7fffffffU 
-                                            & vlSelf->ysyx_24090015_top__DOT__src1) 
-                                           >> (0x1fU 
-                                               & vlSelf->ysyx_24090015_top__DOT__imm))))
+                                  ? ((vlSelf->ysyx_24090015_top__DOT__src1 
+                                      >> 0x1fU) ? (
+                                                   ((0x1fU 
+                                                     >= 
+                                                     ((IData)(0x1fU) 
+                                                      - 
+                                                      (0x1fU 
+                                                       & vlSelf->ysyx_24090015_top__DOT__imm)))
+                                                     ? 
+                                                    ((IData)(0xffffffffU) 
+                                                     << 
+                                                     ((IData)(0x1fU) 
+                                                      - 
+                                                      (0x1fU 
+                                                       & vlSelf->ysyx_24090015_top__DOT__imm)))
+                                                     : 0U) 
+                                                   | (vlSelf->ysyx_24090015_top__DOT__src1 
+                                                      >> 
+                                                      (0x1fU 
+                                                       & vlSelf->ysyx_24090015_top__DOT__imm)))
+                                      : (vlSelf->ysyx_24090015_top__DOT__src1 
+                                         >> (0x1fU 
+                                             & vlSelf->ysyx_24090015_top__DOT__imm)))
                                   : ((0x4013U == (0x707fU 
                                                   & vlSelf->inst))
                                       ? (vlSelf->ysyx_24090015_top__DOT__src1 
--- npc/obj_dir/Vysyx_24090015_top___024root__DepSet_h4c88153f__0__Slow.cpp
+++ npc/obj_dir/Vysyx_24090015_top___024root__DepSet_h4c88153f__0__Slow.cpp
@@ -584,12 +584,29 @@ VL_ATTR_COLD void Vysyx_24090015_top___024root___stl_sequent__TOP__0(Vysyx_24090
                                  < vlSelf->ysyx_24090015_top__DOT__imm)
                               : ((0x40005013U == (0xfe00707fU 
                                                   & vlSelf->inst))
-                                  ? ((0x80000000U & vlSelf->ysyx_24090015_top__DOT__src1) 
-                                     | (0x7fffffffU 
-                                        & ((0x7fffffffU 
-                                            & vlSelf->ysyx_24090015_top__DOT__src1) 
-                                           >> (0x1fU 
-                                               & vlSelf->ysyx_24090015_top__DOT__imm))))
+                                  ? ((vlSelf->ysyx_24090015_top__DOT__src1 
+                                      >> 0x1fU) ? (
+                                                   ((0x1fU 
+                                                     >= 
+                                                     ((IData)(0x1fU) 
+                                                      - 
+                                                      (0x1fU 
+                                                       & vlSelf->ysyx_24090015_top__DOT__imm)))
+                                                     ? 
+                                                    ((IData)(0xffffffffU) 
+                                                     << 
+                                                     ((IData)(0x1fU) 
+                                                      - 
+                                                      (0x1fU 
+                                                       & vlSelf->ysyx_24090015_top__DOT__imm)))
+                                                     : 0U) 
+                                                   | (vlSelf->ysyx_24090015_top__DOT__src1 
+                                                      >> 
+                                                      (0x1fU 
+                                                       & vlSelf->ysyx_24090015_top__DOT__imm)))
+                                      : (vlSelf->ysyx_24090015_top__DOT__src1 
+                                         >> (0x1fU 
+                                             & vlSelf->ysyx_24090015_top__DOT__imm)))
                                   : ((0x4013U == (0x707fU 
                                                   & vlSelf->inst))
                                       ? (vlSelf->ysyx_24090015_top__DOT__src1 
