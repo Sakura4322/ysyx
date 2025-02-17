869bff23329b0d3fea2f7acbeba5dfbb28ffad7b
tracer-ysyx <tracer@ysyx.org>
2025-02-17 23:39:06 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:39:06 up 20:15,  6 users,  load average: 1.53, 1.65, 1.69

--- npc/obj_dir/Vysyx_24090015_top___024root__DepSet_h4c88153f__0.cpp
+++ npc/obj_dir/Vysyx_24090015_top___024root__DepSet_h4c88153f__0.cpp
@@ -631,7 +631,8 @@ VL_INLINE_OPT void Vysyx_24090015_top___024root___ico_sequent__TOP__0(Vysyx_2409
                 = vlSelf->ysyx_24090015_top__DOT__pmem_rdata;
         } else if ((0x4003U == (0x707fU & vlSelf->inst))) {
             vlSelf->ysyx_24090015_top__DOT__rd_wdata 
-                = (0xffU & vlSelf->ysyx_24090015_top__DOT__pmem_rdata);
+                = (vlSelf->ysyx_24090015_top__DOT__pmem_rdata 
+                   >> 0x18U);
         } else if ((0x17U == (0x7fU & vlSelf->inst))) {
             vlSelf->ysyx_24090015_top__DOT__rd_wdata 
                 = (vlSelf->pc + vlSelf->ysyx_24090015_top__DOT__imm);
@@ -1070,7 +1071,8 @@ VL_INLINE_OPT void Vysyx_24090015_top___024root___nba_comb__TOP__0(Vysyx_2409001
                 = vlSelf->ysyx_24090015_top__DOT__pmem_rdata;
         } else if ((0x4003U == (0x707fU & vlSelf->inst))) {
             vlSelf->ysyx_24090015_top__DOT__rd_wdata 
-                = (0xffU & vlSelf->ysyx_24090015_top__DOT__pmem_rdata);
+                = (vlSelf->ysyx_24090015_top__DOT__pmem_rdata 
+                   >> 0x18U);
         } else if ((0x17U == (0x7fU & vlSelf->inst))) {
             vlSelf->ysyx_24090015_top__DOT__rd_wdata 
                 = (vlSelf->pc + vlSelf->ysyx_24090015_top__DOT__imm);
