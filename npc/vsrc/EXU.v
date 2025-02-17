985930cc129313456d7f6d38c1a37f7a6ddce6cf
tracer-ysyx <tracer@ysyx.org>
2025-02-17 23:21:27 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:21:27 up 19:57,  6 users,  load average: 1.90, 1.81, 1.63

--- npc/vsrc/EXU.v
+++ npc/vsrc/EXU.v
@@ -117,6 +117,9 @@ module ysyx_24090015_EXU#(WIDTH=32) (
 					32'b0000000_?????_?????_001_?????_01100_11: begin //sll R
 					rd_wdata = {src1[31],src1[30:0]<<src2[4:0]};
 					end
+					32'b0100000_?????_?????_101_?????_01100_11: begin //sra R
+					rd_wdata = (src1[31]==1'b1) ? ((32'hffffffff<<(31-src2[4:0]) | src1>>src2[4:0])) : src1>>src2[4:0];
+					end
 					32'b0000000_?????_?????_011_?????_01100_11: begin //sltu R
                     rd_wdata = (src1<src2);
                     end
