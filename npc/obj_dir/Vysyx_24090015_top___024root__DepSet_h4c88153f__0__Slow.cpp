48905ec5cf1b2b79884f68338710bd8be50e0c07
tracer-ysyx <tracer@ysyx.org>
2025-02-18 09:50:23 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:50:23 up 21:26,  6 users,  load average: 2.83, 1.81, 1.44

--- npc/obj_dir/Vysyx_24090015_top___024root__DepSet_h4c88153f__0__Slow.cpp
+++ npc/obj_dir/Vysyx_24090015_top___024root__DepSet_h4c88153f__0__Slow.cpp
@@ -393,20 +393,29 @@ VL_ATTR_COLD void Vysyx_24090015_top___024root___stl_sequent__TOP__0(Vysyx_24090
                     if ((0x17U != (0x7fU & vlSelf->inst))) {
                         if ((0x37U != (0x7fU & vlSelf->inst))) {
                             if ((0x6fU != (0x7fU & vlSelf->inst))) {
-                                vlSelf->ysyx_24090015_top__DOT__pmem_wdata 
-                                    = vlSelf->ysyx_24090015_top__DOT__src2;
-                                vlSelf->ysyx_24090015_top__DOT__pmem_waddr 
-                                    = ((0x2023U == 
-                                        (0x707fU & vlSelf->inst))
-                                        ? (vlSelf->ysyx_24090015_top__DOT__src1 
-                                           + vlSelf->ysyx_24090015_top__DOT__imm)
-                                        : ((0x1023U 
+                                if ((0x2023U == (0x707fU 
+                                                 & vlSelf->inst))) {
+                                    vlSelf->ysyx_24090015_top__DOT__pmem_wdata 
+                                        = vlSelf->ysyx_24090015_top__DOT__src2;
+                                    vlSelf->ysyx_24090015_top__DOT__pmem_waddr 
+                                        = (vlSelf->ysyx_24090015_top__DOT__src1 
+                                           + vlSelf->ysyx_24090015_top__DOT__imm);
+                                } else if ((0x1023U 
                                             == (0x707fU 
-                                                & vlSelf->inst))
-                                            ? (vlSelf->ysyx_24090015_top__DOT__src1 
-                                               + vlSelf->ysyx_24090015_top__DOT__imm)
-                                            : (vlSelf->ysyx_24090015_top__DOT__src1 
-                                               + vlSelf->ysyx_24090015_top__DOT__imm)));
+                                                & vlSelf->inst))) {
+                                    vlSelf->ysyx_24090015_top__DOT__pmem_wdata 
+                                        = (0xffU & vlSelf->ysyx_24090015_top__DOT__src2);
+                                    vlSelf->ysyx_24090015_top__DOT__pmem_waddr 
+                                        = (vlSelf->ysyx_24090015_top__DOT__src1 
+                                           + vlSelf->ysyx_24090015_top__DOT__imm);
+                                } else {
+                                    vlSelf->ysyx_24090015_top__DOT__pmem_wdata 
+                                        = (0xffffU 
+                                           & vlSelf->ysyx_24090015_top__DOT__src2);
+                                    vlSelf->ysyx_24090015_top__DOT__pmem_waddr 
+                                        = (vlSelf->ysyx_24090015_top__DOT__src1 
+                                           + vlSelf->ysyx_24090015_top__DOT__imm);
+                                }
                             }
                         }
                     }
