ab0c7aa053402631fd7edbdc7da73a3566eaece2
tracer-ysyx <tracer@ysyx.org>
2025-02-22 17:43:29 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:43:29 up 1 day, 13:52,  5 users,  load average: 1.35, 1.17, 1.10

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -306,7 +306,6 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 	for( i=0; i<end;  i++){
 	if (*(fmt+i)=='%') {
 			char temp=*(fmt+i+1);
-			putch(temp);
 			if(temp=='%'){
 				char temp_str[2];
 				temp_str[0]='%';
@@ -410,9 +409,8 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 				}
 
 
-					for(int k=0;k<len_out;k++){
 						strcat(out,temp_str);
-					}
+
 
 
 				if(flags=='-'){
