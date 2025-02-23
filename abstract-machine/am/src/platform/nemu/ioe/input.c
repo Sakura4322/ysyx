17139550e4aa926b9f32dc00b2ab450f893f01ac
tracer-ysyx <tracer@ysyx.org>
2025-02-23 22:16:45 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:16:45 up 2 days,  2:42,  5 users,  load average: 0.88, 0.55, 0.54

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -6,7 +6,7 @@
 extern int printf(const char *fmt, ...);
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   //printf("%08x",KBD_ADDR);
-  kbd->keydown = 1;
+  // kbd->keydown = 0;
 //kbd->keycode = AM_KEY_NONE;
   kbd->keycode = inl(KBD_ADDR);
 }
