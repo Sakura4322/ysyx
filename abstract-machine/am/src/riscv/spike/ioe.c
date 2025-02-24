b7f7041595beb0c8e11a4bb700b0bcdff4d76663
tracer-ysyx <tracer@ysyx.org>
2025-02-24 17:54:59 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:54:59 up 2 days, 11:44,  5 users,  load average: 2.69, 1.73, 1.18

--- abstract-machine/am/src/riscv/spike/ioe.c
+++ abstract-machine/am/src/riscv/spike/ioe.c
@@ -4,6 +4,7 @@
 void __am_timer_init();
 void __am_timer_rtc(AM_TIMER_RTC_T *);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *);
+void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd);
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 
@@ -12,6 +13,7 @@ static void *lut[128] = {
   [AM_TIMER_CONFIG] = __am_timer_config,
   [AM_TIMER_RTC   ] = __am_timer_rtc,
   [AM_TIMER_UPTIME] = __am_timer_uptime,
+  [AM_INPUT_KEYBRD] = __am_input_keybrd,
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
