f6e06d9c5fe9659b19b02bbb777f26b45e21e487
tracer-ysyx <tracer@ysyx.org>
2025-02-24 12:51:53 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:51:53 up 2 days,  6:41,  5 users,  load average: 1.19, 0.97, 0.73

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -64,7 +64,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  printf("map write\n");
+ // printf("map write\n");
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
