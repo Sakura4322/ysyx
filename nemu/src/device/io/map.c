cba73ccb46e8ee94892e9115d9d3a204f7c7f377
tracer-ysyx <tracer@ysyx.org>
2025-02-24 12:50:48 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:50:48 up 2 days,  6:40,  5 users,  load average: 1.03, 0.91, 0.69

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -54,7 +54,7 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  printf("map read\n");
+  // printf("map read\n");
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
