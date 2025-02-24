b444d36fcccf6a6265710afdf060eea17cb1943b
tracer-ysyx <tracer@ysyx.org>
2025-02-24 17:18:18 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:18:18 up 2 days, 11:08,  5 users,  load average: 1.62, 1.05, 0.63

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -55,6 +55,8 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
 
 /* bus interface */
 word_t mmio_read(paddr_t addr, int len) {
+  IOMap *test= fetch_mmio_map(addr);
+  printf("fetch mapio name : %s\n",test->name);
   return map_read(addr, len, fetch_mmio_map(addr));
 }
 
