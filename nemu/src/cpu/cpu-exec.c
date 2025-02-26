d1efeaf5f69df9596ba9d1dd7f15c03258743aa3
tracer-ysyx <tracer@ysyx.org>
2025-02-26 12:21:18 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:21:18 up 2 days, 17:58,  5 users,  load average: 2.45, 1.12, 0.70

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -363,6 +363,8 @@ printf("funcs is : %s start : %08x end: %08x\n",func_addr[i].func_name,func_addr
 	#endif		
 		break;
 	}
+	static int t=1;
+	printf("cpu exec : %d\n",t++);
     IFDEF(CONFIG_DEVICE, device_update());
   }
 }
