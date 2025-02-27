22498e0e90fdf123222aee3dd7ca5bb2ab59aaa2
tracer-ysyx <tracer@ysyx.org>
2025-02-27 20:28:09 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:28:09 up 3 days,  8:58,  5 users,  load average: 0.92, 0.96, 0.88

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -13,6 +13,7 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  printf("HERE IS CONFIG\n");
   uint32_t get_width = (uint32_t)inw(VGACTL_ADDR+2);
   uint32_t get_height = (uint32_t)inw(VGACTL_ADDR);
 
