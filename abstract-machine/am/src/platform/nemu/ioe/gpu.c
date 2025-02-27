72fa848b32f8301f31a1c621e951af54c093a2a8
tracer-ysyx <tracer@ysyx.org>
2025-02-27 17:58:25 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:58:25 up 3 days,  6:28,  5 users,  load average: 1.08, 1.20, 1.01

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -33,7 +33,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int cnt=0;
   for(int i=0;i<ctl->h;i++){
     for(int j=0;j<ctl->w;j++){
-      int addr=(get_width*(ctl->y+i)+ctl->x+j)*sizeof(uint32_t);
+      int addr=FB_ADDR+(get_width*(ctl->y+i)+ctl->x+j)*sizeof(uint32_t);
       printf("%d\taddr : %08x\n",cnt,addr);
       outl(addr,pixels_ptr[cnt]);
       cnt++;
