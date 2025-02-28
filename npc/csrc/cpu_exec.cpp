9e8fe63f9cd5486082d6b04e473753bf93b01a56
tracer-ysyx <tracer@ysyx.org>
2025-02-28 20:29:12 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:29:12 up 3 days, 16:37,  5 users,  load average: 1.08, 0.74, 0.75

--- npc/csrc/cpu_exec.cpp
+++ npc/csrc/cpu_exec.cpp
@@ -93,9 +93,9 @@ if(((s->inst & 0b00000000000000000000000001111111) == 0b000000000000000000000000
         for(int i = 0; i < cnt_func_num; i++){
 					// printf("0x%08x : ",s->pc);
             if(s->pc >= func_addr[i].start && s->pc < func_addr[i].end){
-							for(int i=0;i<rsp;i++){
-							printf(" ");	
-							}
+							// for(int i=0;i<rsp;i++){
+							// printf(" ");	
+							// }
                 // printf("ret %s\trsp : %d \n",  func_addr[i].func_name, rsp-1);	
 			    //       printf("addr[rsp] : %08x\t s->dnpc : %08x \n",addr[i],s->dnpc);
                 log_write("0x%08x: ret [%s @ 0x%08x]\n", s->pc, func_addr[i].func_name, s->dnpc);
@@ -113,9 +113,9 @@ if(((s->inst & 0b00000000000000000000000001111111) == 0b000000000000000000000000
 		  rsp++;
 			addr[rsp]=s->pc+4;
 			// printf("0x%08x : ",s->pc);
-			for(int i=0;i<rsp;i++){
-							printf(" ");	
-							}
+			// for(int i=0;i<rsp;i++){
+			// 				printf(" ");	
+			// 				}
 			// printf("call %s\trsp : %d \n",func_addr[i].func_name,rsp);	
 			// printf("addr[rsp] : %08x\t s->dnpc : %08x \n",addr[i],s->dnpc);
 		  //printf("ARE YOU OK??\n");
