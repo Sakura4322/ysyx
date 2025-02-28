786789a026caadf285d8928fc231c4ca2cbccc0c
tracer-ysyx <tracer@ysyx.org>
2025-02-28 21:23:48 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:48 up 3 days, 17:32,  5 users,  load average: 1.37, 1.37, 1.24

--- npc/csrc/cpu_exec.cpp
+++ npc/csrc/cpu_exec.cpp
@@ -80,10 +80,10 @@ Addr_Imfo *read_sym_func(){//all func info in func_addr
 
 
 static void ftrace(Addr_Imfo *func_addr,Decode *s){
-static uint32_t addr[20000];	
+static uint32_t addr[200000];	
 static int rsp=0;
 //Assert(rsp<2000,"\n\n\n\n\n\n\nStack Overflow !!!!!!!\n\n\n\n\n\n");	
-if(rsp>=20000){
+if(rsp>=200000){
 	printf("\n\n\n\n\n\n\nStack Overflow !!!!!!!\n\n\n\n\n\n");
 	return ;
 	}
