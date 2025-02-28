361fa14b9034843e17507b6f64ed5faffaf5ba06
tracer-ysyx <tracer@ysyx.org>
2025-02-28 20:14:01 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:14:01 up 3 days, 16:22,  5 users,  load average: 1.39, 1.02, 0.97

--- npc/csrc/DPI.cpp
+++ npc/csrc/DPI.cpp
@@ -78,7 +78,7 @@ if(raddr<0x80000000||raddr>0xffffffff){
 
 if(raddr==RTC_ADDR){
 
-	log_write("dtrace_serial\taddr : %08x\tdata : %02x\n",raddr,temp[0]);
+	log_write("dtrace_serial\taddr : %08x\tdata\n",raddr);
 	return 0;
 }
 log_write("pmem_read\taddr : %08x\tdata : %08x\n",raddr,*(uint32_t *)(vaddr + (raddr -CONFIG_MBASE)));
