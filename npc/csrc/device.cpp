201cf2a88e7f020b47728d436edc053dc4dbf03f
tracer-ysyx <tracer@ysyx.org>
2025-02-28 21:11:43 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:11:43 up 3 days, 17:20,  5 users,  load average: 1.39, 1.36, 1.31

--- npc/csrc/device.cpp
+++ npc/csrc/device.cpp
@@ -4,8 +4,8 @@
 
 
 uint64_t get_time(){
-  struct timeval now;
-  gettimeofday(&now, NULL);
-  uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
+  struct timespec now;
+  clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
+  uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
   return us;
 }
