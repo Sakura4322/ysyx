3e1471a21e2a8fe5c4b5d1361cde4861ed41ebb3
tracer-ysyx <tracer@ysyx.org>
2025-03-17 16:35:39 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:35:39 up  6:42,  6 users,  load average: 5.98, 5.38, 4.35

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -73,7 +73,7 @@ static word_t *read_csrs(uint32_t imm){
 }
 
 #define CSR(i) *read_csrs(i)
-// #define ECALL(dnpc) isa_raise_intr(word_t NO, vaddr_t epc)
+#define ECALL(dnpc) dnpc=isa_raise_intr(R(17), s->pc)
 
 static int decode_exec(Decode *s) {
   int rd = 0;
@@ -152,7 +152,7 @@ INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, word_t t=CSR(imm) 
 // INSTPAT("??????? ????? ????? 101 ????? 11100 11", csrrwi , I, );
 
 INSTPAT("0011000 00010 00000 000 00000 11100 11", mret , R, s->dnpc = cpu.csrs.mepc);
-INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, cpu.csrs.mepc=s->dnpc;cpu.csrs.mcause = R(17);printf("%08x : 写入异常号 ： %08x,dnpc : %08x \n",s->pc,cpu.csrs.mcause,cpu.csrs.mtvec);s->dnpc = cpu.csrs.mtvec);
+INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, ECALL(s->dnpc);printf("%08x : 写入异常号 ： %08x,dnpc : %08x \n",s->pc,cpu.csrs.mcause,cpu.csrs.mtvec));
 INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
 INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
 INSTPAT_END();
