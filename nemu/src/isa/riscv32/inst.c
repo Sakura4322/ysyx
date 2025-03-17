13f86ade57e678c4f402004acc20f11c2f8bbd1c
tracer-ysyx <tracer@ysyx.org>
2025-03-17 16:41:51 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:41:51 up  6:48,  6 users,  load average: 3.70, 4.21, 4.17

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -73,7 +73,7 @@ static word_t *read_csrs(uint32_t imm){
 }
 
 #define CSR(i) *read_csrs(i)
-#define ECALL(dnpc) dnpc=isa_raise_intr(R(17), s->pc)
+#define ECALL(dnpc) dnpc=isa_raise_intr(R(17), dnpc)
 
 static int decode_exec(Decode *s) {
   int rd = 0;
