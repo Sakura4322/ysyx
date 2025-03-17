b99cf0b66e0879912fc92803cca30a19f6f4ed3e
tracer-ysyx <tracer@ysyx.org>
2025-03-17 16:24:57 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:24:57 up  6:31,  6 users,  load average: 4.76, 4.06, 3.28

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -53,7 +53,7 @@ void init_mem() {
 word_t paddr_read(paddr_t addr, int len) {
 // #if   defined(CONFIG_MTRACE)
 // static uint8_t *pmem = NULL;
-	log_write("paddr_read :%08x : %08x\n",(int)addr,(int)pmem_read(addr, len));
+	// log_write("paddr_read :%08x : %08x\n",(int)addr,(int)pmem_read(addr, len));
 // #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -63,7 +63,7 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
 // #if   defined(CONFIG_MTRACE)
-	log_write("paddr_write :%08x : %08x\n",(int)addr,(int)data);
+	// log_write("paddr_write :%08x : %08x\n",(int)addr,(int)data);
 // #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
