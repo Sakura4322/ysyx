2fb332287c8b4c06241c3885a664bdbb5f4990ce
tracer-ysyx <tracer@ysyx.org>
2025-03-21 21:13:29 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:13:29 up 21:10,  6 users,  load average: 1.03, 1.00, 1.07

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -35,7 +35,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *c = kstack.start;
   printf("start : %x\n",kstack.start);
-  asm volatile("mv sp, %0" : : "r"((uintptr_t)kstack.start));
+  asm volatile("mv sp, %0" : : "r"(kstack.start));
   printf("enrty : %x\n",entry);
   asm volatile("jalr %0" : : "r"(entry));
   
