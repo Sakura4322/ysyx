0d7d4a0e412954649c7ce718fc9e3dd5227f690c
tracer-ysyx <tracer@ysyx.org>
2025-08-04 21:30:07 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:30:06 up 20 min,  5 users,  load average: 7.55, 6.53, 5.45

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -38,7 +38,9 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   // printf("enrty : %x\n",entry);
   asm volatile("mv sp, %0" : : "r"(kstack.end-sizeof(Context)));
   
-  c->mepc = (uintptr_t)entry - 4;
+  int offset_epc = 4 * 18; // 偏移量
+
+  asm volatile("sw %1, %0(sp)" : : "i"(offset_epc), "r"(entry));
 
   
   return c;
