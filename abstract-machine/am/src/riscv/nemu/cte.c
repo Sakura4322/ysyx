f008d09b8cf8b42af7281a71e6d531ee57611104
tracer-ysyx <tracer@ysyx.org>
2025-08-06 18:49:37 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:49:37 up 19:08, 26 users,  load average: 0.74, 1.05, 1.15

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -16,7 +16,10 @@ Context* __am_irq_handle(Context *c) {
     assert(ev.event != EVENT_ERROR);
 		putch('q');
     assert(c != NULL);
-		printf("preav addr : %x\n",(uintptr_t)c);
+		for(int i=0;i<30;i++){
+
+		printf("gpr[%d] : %x\n",i,(uintptr_t)c->gpr[i]);
+		}
     c = user_handler(ev, c);
     assert(c != NULL);
   }
