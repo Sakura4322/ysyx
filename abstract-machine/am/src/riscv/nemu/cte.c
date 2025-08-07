fe8a5a7a2d2ca82c59286e4f88231a6e9858dcfc
tracer-ysyx <tracer@ysyx.org>
2025-08-07 19:37:42 +0800

    > compile NEMU ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:37:42 up  9:43, 10 users,  load average: 1.32, 0.68, 0.37

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,22 +5,14 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-	// printf("%d,%d,%d,%d,%d,%d\n",1,2,3,4,5,6);
-   //printf("irq_handle : \nmcause : %x\n mstatus : %x\n mepc : %x\n",c->mcause,c->mstatus,c->mepc);
+  // printf("irq_handle : \n\tmcause : %08x\n\tmstatus : %08x\n\tmepc : %08x\n",c->mcause,c->mstatus,c->mepc);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 0xb: ev.event = EVENT_YIELD;
-								c->mepc +=4;
-								break;
+      case -1: ev.event = EVENT_YIELD;break;
       default: ev.event = EVENT_ERROR; printf("Unkonw event\n");break;
     }
     assert(ev.event != EVENT_ERROR);
-    assert(c != NULL);
-		//for(int i=0;i<30;i++){
-
-		//printf("gpr[%d] : %x\n",i,(uintptr_t)c->gpr[i]);
-		//}
     c = user_handler(ev, c);
     assert(c != NULL);
   }
@@ -30,10 +22,9 @@ Context* __am_irq_handle(Context *c) {
 extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
-  //printf("cte_init\n");
+  printf("cte_init\n");
   // initialize exception entry
-  int ini_value = 0x1800;
-  asm volatile("csrw mstatus, %0" : : "r"(ini_value));    //给mstatus 初始化为0x1800来通过difftest
+  asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
   // register event handler
   user_handler = handler;
@@ -42,20 +33,15 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-	struct Context *context = (struct Context*)kstack.end - sizeof(struct Context);
-	memset(context,0,sizeof(struct Context));
-	context->mstatus = 0x1800;
-	context->gpr[10] = (uintptr_t)arg;
-	context->mepc = (uintptr_t)entry;
-  return context;
+  return NULL;
 }
 
 void yield() {
-   //printf("yield\n");
+  // printf("yield\n");
 #ifdef __riscv_e
   asm volatile("li a5, -1; ecall");
 #else
-  asm volatile("li a7, 0xb; ecall");
+  asm volatile("li a7, -1; ecall");
 #endif
 }
 
