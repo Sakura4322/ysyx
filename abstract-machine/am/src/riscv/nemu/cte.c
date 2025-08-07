caf9e6003a9298fb03c106081f0b1c761e08da33
陈广钊 <3450656190@qq.com>
2025-08-07 19:58:00 +0800

    成功进行线程转换，但是会栈溢出

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case -1: ev.event = EVENT_YIELD;break;
+      case 0xd: ev.event = EVENT_YIELD;break;
       default: ev.event = EVENT_ERROR; printf("Unkonw event\n");break;
     }
     assert(ev.event != EVENT_ERROR);
@@ -22,7 +22,7 @@ Context* __am_irq_handle(Context *c) {
 extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
-  printf("cte_init\n");
+//  printf("cte_init\n");
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
@@ -33,15 +33,19 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
-}
+	struct Context* context = kstack.end - sizeof(struct Context);
+	context->mstatus = 0x1800;
+	context->mepc = (uintptr_t)entry;                                                                                                                               
+	context->gpr[10] = (uintptr_t)arg;
+return context;
+	 }
 
 void yield() {
   // printf("yield\n");
 #ifdef __riscv_e
-  asm volatile("li a5, -1; ecall");
+  asm volatile("li a5, 0xd; ecall");
 #else
-  asm volatile("li a7, -1; ecall");
+  asm volatile("li a7, 0xd; ecall");
 #endif
 }
 
