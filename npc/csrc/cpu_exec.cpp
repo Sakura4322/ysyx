2197f8fc5bee2f0b05f3b1df71c21d4f42f43fc4
tracer-ysyx <tracer@ysyx.org>
2025-08-09 19:04:06 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:04:06 up 1 day,  7:40, 18 users,  load average: 2.42, 1.99, 1.72

--- npc/csrc/cpu_exec.cpp
+++ npc/csrc/cpu_exec.cpp
@@ -159,30 +159,30 @@ static void execute(uint64_t n) {
     if (top->flag)npc_state.state=NPC_END;
     
 
-//trace_and_difftest(&s, cpu.pc);
+trace_and_difftest(&s, cpu.pc);
 
 
-// if(n%2==0){
-// for (int i=0;i<32;i++){//storage reg information
-// 		char buf_temp[16]={0};
-// 		sprintf(buf_temp,"%s : %08x\n",regs[i],cpu.gpr[i]);	
-// 		strcat(buf,buf_temp);
-//  	}
+if(n%2==0){
+for (int i=0;i<32;i++){//storage reg information
+		char buf_temp[16]={0};
+		sprintf(buf_temp,"%s : %08x\n",regs[i],cpu.gpr[i]);	
+		strcat(buf,buf_temp);
+ 	}
  	
  	
-// 		strcpy(*(iringbuf_reg_state+cout_pc_num%2),buf);//use iringbuf storage the reg information
-// 	  iring_load(iringbuf,&s,cout_pc_num++);//storage the information of instructions		
+		strcpy(*(iringbuf_reg_state+cout_pc_num%2),buf);//use iringbuf storage the reg information
+	  iring_load(iringbuf,&s,cout_pc_num++);//storage the information of instructions		
 
 // ftrace
 
-//     ftrace(func_addr,&s);
+    ftrace(func_addr,&s);
 
 
 
 // itrace
-// 		ret == 0 is good trap  or bad trap
+		ret == 0 is good trap  or bad trap
 			
-// 	}
+	}
 	
 	
 	if (!npc_state.halt_ret&& npc_state.state!=NPC_RUNNING){
