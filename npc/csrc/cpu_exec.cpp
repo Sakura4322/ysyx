115dc8a51ae73582cde9f99783558fa2a03f35fc
tracer-ysyx <tracer@ysyx.org>
2025-09-10 21:06:39 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:06:39 up  8:38,  6 users,  load average: 1.75, 1.27, 1.48

--- npc/csrc/cpu_exec.cpp
+++ npc/csrc/cpu_exec.cpp
@@ -164,22 +164,22 @@ Addr_Imfo *func_addr = read_sym_func();
 
 
 if(n%2==0){
-	char buf_temp[32]={0};
-for (int i=0;i<32;i++){//storage reg information
-		sprintf(buf_temp,"%s : %08x\n",regs[i],cpu.gpr[i]);	
-		strcat(buf,buf_temp);
- 	}
-	 sprintf(buf_temp,"mstatus : %08x\n",cpu.csr[0]);	
-	 strcat(buf,buf_temp);
-	 sprintf(buf_temp,"mtvec   : %08x\n",cpu.csr[1]);	
-	 strcat(buf,buf_temp);
-	 sprintf(buf_temp,"mepc    : %08x\n",cpu.csr[2]);	
-	 strcat(buf,buf_temp);
-	 sprintf(buf_temp,"%mcause : %08x\n",cpu.csr[3]);	
-	 strcat(buf,buf_temp);
+// 	char buf_temp[32]={0};
+// for (int i=0;i<32;i++){//storage reg information
+// 		sprintf(buf_temp,"%s : %08x\n",regs[i],cpu.gpr[i]);	
+// 		strcat(buf,buf_temp);
+//  	}
+// 	 sprintf(buf_temp,"mstatus : %08x\n",cpu.csr[0]);	
+// 	 strcat(buf,buf_temp);
+// 	 sprintf(buf_temp,"mtvec   : %08x\n",cpu.csr[1]);	
+// 	 strcat(buf,buf_temp);
+// 	 sprintf(buf_temp,"mepc    : %08x\n",cpu.csr[2]);	
+// 	 strcat(buf,buf_temp);
+// 	 sprintf(buf_temp,"%mcause : %08x\n",cpu.csr[3]);	
+// 	 strcat(buf,buf_temp);
  	
-	strcpy(*(iringbuf_reg_state+cout_pc_num%2),buf);//use iringbuf storage the reg information
-	iring_load(iringbuf,&s,cout_pc_num++);//storage the information of instructions		
+// 	strcpy(*(iringbuf_reg_state+cout_pc_num%2),buf);//use iringbuf storage the reg information
+// 	iring_load(iringbuf,&s,cout_pc_num++);//storage the information of instructions		
 
 // ftrace
 
