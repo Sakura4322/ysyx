204fe96266850d33f7d7abac507b1c121abd6d19
tracer-ysyx <tracer@ysyx.org>
2025-09-10 20:14:27 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:14:27 up  7:46,  6 users,  load average: 2.33, 1.73, 1.47

--- npc/csrc/device/io/mmio.c
+++ npc/csrc/device/io/mmio.c
@@ -7,21 +7,21 @@
 static IOMap maps[NR_MAP] = {};
 static int nr_map = 0;
 
-static IOMap* fetch_mmio_map(uint32_t addr) {
+static IOMap* fetch_mmio_map(unsigned int addr) {
   int mapid = find_mapid_by_addr(maps, nr_map, addr);
   return (mapid == -1 ? NULL : &maps[mapid]);
 }
 
-static void report_mmio_overlap(const char *name1, uint32_t l1, uint32_t r1,
-    const char *name2, uint32_t l2, uint32_t r2) {
+static void report_mmio_overlap(const char *name1, unsigned int l1, unsigned int r1,
+    const char *name2, unsigned int l2, unsigned int r2) {
   panic("MMIO region %s@[" FMT_PADDR ", " FMT_PADDR "] is overlapped "
                "with %s@[" FMT_PADDR ", " FMT_PADDR "]", name1, l1, r1, name2, l2, r2);
 }
 
 /* device interface */
-void add_mmio_map(const char *name, uint32_t addr, void *space, uint32_t len, io_callback_t callback) {
+void add_mmio_map(const char *name, unsigned int addr, void *space, unsigned int len, io_callback_t callback) {
   assert(nr_map < NR_MAP);
-  uint32_t left = addr, right = addr + len - 1;
+  unsigned int left = addr, right = addr + len - 1;
   if (in_pmem(left) || in_pmem(right)) {
     report_mmio_overlap(name, left, right, "pmem", PMEM_LEFT, PMEM_RIGHT);
   }
@@ -40,14 +40,14 @@ void add_mmio_map(const char *name, uint32_t addr, void *space, uint32_t len, io
 }
 
 /* bus interface */
-uint32_t mmio_read(uint32_t addr, int len) {
+unsigned int mmio_read(unsigned int addr, int len) {
   // IOMap *test= fetch_mmio_map(addr);
   // if(test!=NULL)
   // {printf("mmio_read fetch mapio name : %s\n",test->name);}
   return map_read(addr, len, fetch_mmio_map(addr));
 }
 
-void mmio_write(uint32_t addr, int len, uint32_t data) {
+void mmio_write(unsigned int addr, int len, unsigned int data) {
   // IOMap *test= fetch_mmio_map(addr);
   // if(test!=NULL)
   // printf("mmoi_write fetch mapio name : %s\n",test->name);
--- npc/csrc/map.h
+++ npc/csrc/map.h
@@ -3,23 +3,23 @@
 void    difftest_skip_ref();
 
 
-typedef void(*io_callback_t)(uint32_t, int, bool);
+typedef void(*io_callback_t)(unsigned int, int, bool);
 uint8_t* new_space(int size);
 
 typedef struct {
   const char *name;
-  // we treat ioaddr_t as uint32_t here
-  uint32_t low;
-  uint32_t high;
+  // we treat ioaddr_t as unsigned int here
+  unsigned int low;
+  unsigned int high;
   void *space;
   io_callback_t callback;
 } IOMap;
 
-static inline bool map_inside(IOMap *map, uint32_t addr) {
+static inline bool map_inside(IOMap *map, unsigned int addr) {
   return (addr >= map->low && addr <= map->high);
 }
 
-static inline int find_mapid_by_addr(IOMap *maps, int size, uint32_t addr) {
+static inline int find_mapid_by_addr(IOMap *maps, int size, unsigned int addr) {
   int i;
   for (i = 0; i < size; i ++) {
     if (map_inside(maps + i, addr)) {
@@ -30,12 +30,12 @@ static inline int find_mapid_by_addr(IOMap *maps, int size, uint32_t addr) {
   return -1;
 }
 
-void add_pio_map(const char *name, uint32_t addr,
-        void *space, uint32_t len, io_callback_t callback);
-void add_mmio_map(const char *name, uint32_t addr,
-        void *space, uint32_t len, io_callback_t callback);
+void add_pio_map(const char *name, unsigned int addr,
+        void *space, unsigned int len, io_callback_t callback);
+void add_mmio_map(const char *name, unsigned int addr,
+        void *space, unsigned int len, io_callback_t callback);
 
-uint32_t map_read(uint32_t addr, int len, IOMap *map);
-void map_write(uint32_t addr, int len, uint32_t data, IOMap *map);
+unsigned int map_read(unsigned int addr, int len, IOMap *map);
+void map_write(unsigned int addr, int len, unsigned int data, IOMap *map);
 
 #endif
