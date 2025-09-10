1f2d31795bdc38e76d755609ca6a5e016ca01d0a
tracer-ysyx <tracer@ysyx.org>
2025-09-11 00:47:44 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:47:44 up 12:19,  6 users,  load average: 0.93, 1.01, 1.50

--- npc/csrc/device/serial.cpp
+++ npc/csrc/device/serial.cpp
@@ -14,7 +14,7 @@ static void serial_io_handler(uint32_t offset, int len, bool is_write) {
       /* We bind the serial port with the host stderr in NEMU. */
       case CH_OFFSET:
         if (is_write) putchar(serial_base[0]);
-        else log_write("do not support read");
+        else log_write("do not support read\n");
         break;
       default: log_write("do not support offset = %d", offset);
     }
