c0d0132d3be16bf982fe0297daa0cd38acac04b5
tracer-ysyx <tracer@ysyx.org>
2025-09-10 15:29:35 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:29:35 up  3:01,  6 users,  load average: 2.48, 1.85, 1.30

--- npc/csrc/difftest.h
+++ npc/csrc/difftest.h
@@ -3,4 +3,5 @@
 
 
 extern void difftest_step(uint32_t pc, uint32_t npc);
+extern void difftest_skip_ref(uint32_t pc, uint32_t npc);
 extern void init_difftest(char *ref_so_file, long img_size, int port);
