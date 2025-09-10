df1b41a345c1ff40a530e87ae4c011e9069bfd68
tracer-ysyx <tracer@ysyx.org>
2025-09-10 14:33:25 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:33:25 up  2:05,  6 users,  load average: 1.60, 1.34, 1.14

--- npc/csrc/DPI.cpp
+++ npc/csrc/DPI.cpp
@@ -110,8 +110,9 @@ extern "C" void pmem_write(int waddr,int wdata,char wmask){
 
 	if(waddr>=SERIAL_PORT && waddr < FB_ADDR){
 		
-		// putchar(temp[0]);
+		putchar(temp[0]);
 		log_write("dtrace_serial\taddr : %08x\tdata : %c\n",waddr,temp[0]);
+		return;
 	}else if(waddr>=VGACTL_ADDR && waddr < AUDIO_ADDR ){
 		log_write("dtrace_vga\taddr : %08x\tdata : %c\n",waddr,temp[0]);
 	}else if(waddr>=FB_ADDR && waddr < AUDIO_SBUF_ADDR ){
