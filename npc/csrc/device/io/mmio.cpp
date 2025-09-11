4f7cad524cd2974323008ac6f2cb848e1479e893
tracer-ysyx <tracer@ysyx.org>
2025-09-11 11:37:54 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:37:54 up 13:48,  6 users,  load average: 2.48, 1.41, 1.36

--- npc/csrc/device/io/mmio.cpp
+++ npc/csrc/device/io/mmio.cpp
@@ -34,8 +34,8 @@ void add_mmio_map(const char *name, uint32_t addr, void *space, uint32_t len, io
 
   maps[nr_map] = (IOMap){ .name = name, .low = addr, .high = addr + len - 1,
     .space = space, .callback = callback };
-  log_write("Add mmio map '%s' at [%08x, %08x]\n",
-      maps[nr_map].name, maps[nr_map].low, maps[nr_map].high);
+  log_write("Add mmio map '%s' at [%08x, %08x] size[%d]\n",
+      maps[nr_map].name, maps[nr_map].low, maps[nr_map].high,len);
 
   nr_map ++;
 }
