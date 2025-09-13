fded3b179a221f7b0fc89ae0fe89fd4fa0532431
tracer-ysyx <tracer@ysyx.org>
2025-09-13 23:50:30 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:50:30 up 10:03,  5 users,  load average: 3.92, 2.55, 1.74

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -6,8 +6,8 @@ void __am_timer_init() {
 // extern int printf(const char *fmt, ...);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   // printf("%08x\n",RTC_ADDR);
-  uint32_t lo = inl(RTC_ADDR);
   uint32_t hi = inl(RTC_ADDR+4);
+  uint32_t lo = inl(RTC_ADDR);
   uptime->us = ((uint64_t)hi<<32) | lo;
 }
 
