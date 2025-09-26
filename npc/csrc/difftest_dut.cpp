aec3d6f4de316e3d9efb3946ed90151a4a708a95
tracer-ysyx <tracer@ysyx.org>
2025-09-26 19:36:44 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:36:44 up 10:42,  5 users,  load average: 1.03, 0.69, 0.69

--- npc/csrc/difftest_dut.cpp
+++ npc/csrc/difftest_dut.cpp
@@ -31,7 +31,12 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 
   void *handle;
   handle = dlopen(ref_so_file, RTLD_LAZY);
-  assert(handle);
+  // assert(handle);
+  if (!handle) {
+    fprintf(stderr, "dlopen error: %s\n", dlerror());
+    exit(EXIT_FAILURE);
+}
+
   ref_difftest_memcpy = reinterpret_cast<void (*)(uint32_t, uint8_t*, size_t, bool)>(dlsym(handle, "difftest_memcpy"));
   assert(ref_difftest_memcpy);
   ref_difftest_regcpy = reinterpret_cast<void (*)(CPU_state*, bool)>(dlsym(handle, "difftest_regcpy"));
