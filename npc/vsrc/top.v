b23d373d5da8268d1c7d21b537387b2100d64094
Sakura4322 <3450656190@qq.com>
2025-09-26 13:31:48 +0800

    修改一系列错误

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -21,7 +21,7 @@ module ysyx_24090015_top#(
   WIDTH=32
   ) (
     input clk,
-    input [WIDTH-1:0] inst,
+    output [WIDTH-1:0] inst,
     output reg [WIDTH-1:0] pc,dnpc,
 		output reg flag,
 		output hit_good_or_bad//实现HIT GOOD/BAD的功能
@@ -119,7 +119,7 @@ ysyx_24090015_SRAM sram0(
 )
 
     // 信号声明
-    wire [ADDRWIDTH-1:0] dnpc;
+    // wire [ADDRWIDTH-1:0] dnpc;
     // wire [DATAWIDTH-1:0] inst;
 
     wire [DATAWIDTH-1:0] imm;
