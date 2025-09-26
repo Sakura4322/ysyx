581e93ae0bdd50ed89f3e90bc0f864f734554466
tracer-ysyx <tracer@ysyx.org>
2025-09-26 20:30:10 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:30:10 up 11:36,  6 users,  load average: 0.29, 0.64, 0.88

--- npc/obj_dir/Vysyx_24090015_top___024root__DepSet_h4c88153f__0.cpp
+++ npc/obj_dir/Vysyx_24090015_top___024root__DepSet_h4c88153f__0.cpp
@@ -222,6 +222,7 @@ VL_INLINE_OPT void Vysyx_24090015_top___024root___nba_sequent__TOP__0(Vysyx_2409
         if (vlSelf->ysyx_24090015_top__DOT__ifu0__DOT__ifu_state) {
             if (vlSelf->ysyx_24090015_top__DOT__ifu0__DOT__ifu_state) {
                 if ((1U & vlSelf->ysyx_24090015_top__DOT____Vcellout__sram0__ifu_respValid)) {
+                    vlSelf->ysyx_24090015_top__DOT__ifu_reqValid = 0U;
                     vlSelf->ysyx_24090015_top__DOT__ifu0__DOT__ifu_state = 0U;
                 }
             }
