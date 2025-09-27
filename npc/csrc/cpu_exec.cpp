5d0ded3e13aadc42bb3222966a7be11309e2f864
tracer-ysyx <tracer@ysyx.org>
2025-09-27 11:52:01 +0800

    > sim RTL ysyx_22040000 陈广钊 Linux sakura-Vm 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:52:01 up 16:16,  6 users,  load average: 1.49, 1.37, 1.26

--- npc/csrc/cpu_exec.cpp
+++ npc/csrc/cpu_exec.cpp
@@ -156,7 +156,10 @@ Addr_Imfo *func_addr = read_sym_func();
 	if(diff_on){
 		// static int cnt_fuck=0;			//nemu 运行也和npc 一样clk=1时等待，clk=0时运行 
 		// cnt_fuck++;
-		if(top->fetch && cout_inst_times >= 4)difftest_step(s.pc,s.dnpc);
+		if(top->fetch && cout_inst_times >= 4){
+			printf("s->dnpc : %08x\n\n",s->dnpc);
+			difftest_step(s.pc,s.dnpc);
+		}
 	}
     if (top->flag)npc_state.state=NPC_END;
     
